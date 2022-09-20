1646f1758284b8842307c8e166f827d9a961c7ee
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2022-09-20 23:54:11 +0900

    ch1 연습문제

---
+++ 자료 구조와 함께 배우는 알고리즘 입문 자바편/ch1 기본 알고리즘/Min3.java
@@ -0,0 +1,22 @@
+/*
+ * 연습문제 Q2
+ * 세 값의 최솟값을 구하는 min3 메서드를 작성하세요.
+ */
+
+public class min3 {
+	static int min3(int a, int b, int c) {
+		int min = a;
+		
+		if (b < min) min = b;
+		if (c < min) min = c;
+		
+		return min;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("min3(1,2,3) = " + min3(1,2,3));
+		System.out.println("min3(5,2,3) = " + min3(5,2,3));
+		System.out.println("min3(3,7,3) = " + min3(3,7,3));
+		System.out.println("min3(6,2,2) = " + min3(6,2,2));
+	}
+}
