429d8d96a4ccccdde8f9b543b55a9e7f3934c555
Sieun Park <psieun0606@gmail.com>
2022-11-26 15:14:06 +0900

    feat: [java] 중복된 숫자 개수

---
+++ Programmers/Lv. 0/중복된 숫자 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        
+        for (int x : array) {
+            if (x == n) {
+                answer ++;
+            }
+        }
+        return answer;
+    }
+}
