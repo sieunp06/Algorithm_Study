4faea6bb87374acfed0344b8bbf603ff102334b3
Sieun Park <psieun0606@gmail.com>
2022-12-24 23:02:34 +0900

    feat: 10926.java

---
+++ Baekjoon/bronze/10926/10926.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {   
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+        
+        String id = stdIn.nextLine();
+        
+        System.out.println(id + "??!");
+        
+        stdIn.close();
+    }
+}
