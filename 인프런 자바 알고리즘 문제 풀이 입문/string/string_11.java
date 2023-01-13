f13e96d797dd2440b98eeb89a3a8a9c23b1eef97
Sieun Park <psieun0606@gmail.com>
2023-01-13 23:55:41 +0900

    feat: string_11.java

---
+++ 인프런 자바 알고리즘 문제 풀이 입문/string/string_11.java
@@ -0,0 +1,39 @@
+import java.util.*;
+  
+public class Main {
+  static String solution(String text) {
+		String answer = "";
+		
+		int count = 1;
+		
+		for (int i = 0; i < text.length(); i++) {
+			if (i == 0) {
+				answer += text.charAt(0);
+			} else if (text.charAt(i) == text.charAt(i - 1)) {
+				count++;
+				if (i == text.length() - 1) {
+					answer += String.valueOf(count);
+				}
+			} else if (text.charAt(i) != text.charAt(i - 1)) {
+				if (count > 1) {
+					answer += String.valueOf(count);
+					answer += text.charAt(i);
+				} else {
+					answer += text.charAt(i);
+				}
+				count = 1;
+			}
+		}
+		
+		
+		return answer;
+	}
+	
+	public static void main(String[] args){
+	    Scanner StdIn = new Scanner(System.in);
+	    
+	    String text = StdIn.next();	
+	    
+    	System.out.println(solution(text));
+	}
+}
