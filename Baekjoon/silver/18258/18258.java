aaf179360ca22592fc431484024c77426cc95fec
Sieun Park <psieun0606@gmail.com>
2023-01-31 00:20:02 +0900

    feat: 18258.java

--- Baekjoon/silver/18258/18258.java
+++ Baekjoon/silver/18258/18258.java
@@ -1,61 +1,56 @@
-package Algo;
-
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.StringTokenizer;
-import java.util.ArrayList;
 
-public class algo {		
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(bf.readLine());
-		Deque<Integer> Q = new LinkedList<>();
-		ArrayList<Integer> answer = new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-		
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(bf.readLine());
-			
-			while (st.hasMoreTokens()) {
-				String text = st.nextToken();
-				if (text.equals("push")) {
-					Q.offer(Integer.parseInt(st.nextToken()));
-					break;
-				} else if (text.equals("pop")) {
-					Integer item = Q.poll();
-					if (item == null)
-						answer.add(-1);
-					else answer.add(item);
-					break;
-				} else if (text.equals("size")) {
-					answer.add(Q.size());
-					break;
-				} else if (text.equals("empty")) {
-					if (Q.isEmpty()) 
-						answer.add(1);
-					else 
-						answer.add(0);
-					break;
-				} else if (text.equals("front")) {
-					Integer item = Q.peek();
-					if (item == null)
-						answer.add(-1);
-					else answer.add(item);
-					break;
-				} else if (text.equals("back")) {
-					Integer item = Q.peekLast();
-					if (item == null)
-						answer.add(-1);
-					else answer.add(item);
-					break;
-				}
-			}
-		}
-		for (int i = 0; i < answer.size(); i++) 
-			System.out.println(answer.get(i));
+public class Main {
+    public static void main(String[] args) throws IOException {
+    	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        Deque<Integer> dq = new ArrayDeque<>();
+        
+        int N = Integer.parseInt(bf.readLine());
+        
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            String input = st.nextToken();
+            switch (input) {
+            	case "push":
+            		dq.offer(Integer.parseInt(st.nextToken()));
+            		break;
+            	case "pop":
+            		if (dq.isEmpty())
+                        sb.append(-1).append('\n');
+                    else 
+                        sb.append(dq.poll()).append('\n');
+            		break;
+            	case "size":
+            		sb.append(dq.size()).append('\n');
+            		break;
+            	case "empty":
+            		if (dq.isEmpty())
+                        sb.append(1).append('\n');
+                    else sb.append(0).append('\n');
+            		break;
+            	case "front":
+            		if (dq.isEmpty())
+                        sb.append(-1).append('\n');
+                    else 
+                        sb.append(dq.peek()).append('\n');
+            		break;
+            	case "back":
+            		if (dq.isEmpty())
+                        sb.append(-1).append('\n');
+                    else 
+                        sb.append(dq.peekLast()).append('\n');
+            		break;
+
+            }
+            
+        }
+        System.out.println(sb);
     }
 }
