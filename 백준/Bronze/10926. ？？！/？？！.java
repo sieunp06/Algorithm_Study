9adc45254694dad39963cae13767b1334010b7d6
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-04-09 11:04:05 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(bf.readLine()).append("??!");
+
+        System.out.println(sb);
+    }
+}
