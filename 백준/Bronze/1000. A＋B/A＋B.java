1639ff208cabb2072ac8c3bd694644662ef707e2
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-04-09 10:16:35 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        
+        int total = 0;
+        
+        while (st.hasMoreTokens()) 
+            total += Integer.parseInt(st.nextToken());
+        
+        System.out.println(total);
+    }
+}
