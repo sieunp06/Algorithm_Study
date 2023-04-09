689a186ae69fde12a251d71748c432d6c6031d12
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-04-09 11:04:28 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String text = bf.readLine();
+        char[] text_char = text.toCharArray();
+        Queue<Character> q = new LinkedList<>();
+
+        int len = text.length();
+        int answer = 1;
+
+        for (int i = 0; i < len / 2; i++)
+            q.offer(text_char[i]);
+
+        for (int i = len - 1; i >= len - len / 2; i--) {
+            if (text_char[i] != q.poll()) {
+                answer = 0;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
