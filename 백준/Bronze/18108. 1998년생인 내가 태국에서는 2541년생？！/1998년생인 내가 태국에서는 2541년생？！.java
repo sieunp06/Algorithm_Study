13b73e4dd708d0a0560309b04e5430a8d1adfa5f
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-04-09 11:06:25 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write(String.valueOf(Integer.parseInt(bf.readLine()) - 543));
+        bw.flush();
+        bw.close();
+    }
+}
