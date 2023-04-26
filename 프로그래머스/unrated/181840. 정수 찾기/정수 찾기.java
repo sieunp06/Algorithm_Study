6716352f6446e699f47ba361d61d117d2c5fb4c2
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-04-26 18:13:53 +0900

    [unrated] Title: 정수 찾기, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        for (int num: num_list)
+            if (num == n) return 1;
+        return 0;
+    }
+}
