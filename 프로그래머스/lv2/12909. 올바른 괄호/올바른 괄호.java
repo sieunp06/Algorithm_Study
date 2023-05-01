bfc7d8d75a81f9d590209875885ee5776afd6b06
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-05-02 00:24:20 +0900

    [level 2] Title: 올바른 괄호, Time: 16.02 ms, Memory: 52.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        char[] arr = s.toCharArray();
+        Stack<Character> st = new Stack<>();
+        
+        if (arr[0] == ')') return false;
+        
+        for (char alpha: arr) {
+            if (alpha == '(') st.push(alpha);
+            else {
+                if (st.isEmpty()) return false;
+                else st.pop();
+            }
+        }
+        if (!st.isEmpty()) return false;
+        return true;
+    }
+}
