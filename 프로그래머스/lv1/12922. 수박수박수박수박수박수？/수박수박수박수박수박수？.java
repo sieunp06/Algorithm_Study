23361e6330515c22e90829047ba70df8c41e3f5a
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-05-02 11:35:47 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.26 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,17 +2,10 @@ class Solution {
     public String solution(int n) {
         String answer = "";
         
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (count == 0) {
-                answer += "수";
-                count++;
-            }
-            else {
-                answer += "박";
-                count = 0;
-            }
+        for (int i = 1; i <= n; i++) {
+            answer += (i % 2 != 0) ? "수" : "박";
         }
+            
         return answer;
     }
 }
