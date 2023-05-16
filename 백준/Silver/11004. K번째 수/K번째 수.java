cb3e8fa882e178cef7f92df1d5671a091d0252c2
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-05-16 11:04:19 +0900

    [Silver V] Title: K번째 수, Time: 2000 ms, Memory: 374592 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(bf.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] numbers = new int[N];
+
+        st = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(numbers);
+
+        System.out.println(numbers[M - 1]);
+    }
+}
