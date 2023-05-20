10dedc547dde1b0d7841957d84e9f73195293db1
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-05-20 22:00:24 +0900

    [unrated] Title: 뒤에서 5등까지, Time: 0.46 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] num_list) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        
+        Arrays.sort(num_list);
+        
+        for (int i = 0; i < 5; i++) 
+            answer.add(num_list[i]);
+        
+        return answer;
+    }
+}
