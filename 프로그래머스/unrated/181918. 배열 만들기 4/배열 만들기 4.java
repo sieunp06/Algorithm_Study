3370b59e4e3bf97ab309119dc1193d51bccaa3c3
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-09-07 22:50:43 +0900

    [unrated] Title: 배열 만들기 4, Time: 3.85 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+class Solution {
+    public ArrayList<Integer> solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
+        
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i += 1;
+            } else if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i += 1;
+            } else {
+                stk.remove(stk.size() - 1);
+            }
+        }
+        
+        return stk;
+    }
+}
