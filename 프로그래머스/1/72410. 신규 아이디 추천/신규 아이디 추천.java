938444fafdad04034193371295d1b43c447eab99
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2023-12-05 18:15:24 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.22 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,69 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase();
+        StringBuilder sb;
+        
+        sb = new StringBuilder();
+        for (char alpha : answer.toCharArray()) {
+            if (alpha >= 'a' && alpha <= 'z') {
+                sb.append(alpha);
+                continue;
+            } 
+            if (alpha >= '0' && alpha <= '9') {
+                sb.append(alpha);
+                continue;
+            } 
+            if (alpha == '-' || alpha == '_' || alpha == '.') {
+                sb.append(alpha);
+                continue;
+            }
+        }
+        answer = sb.toString();
+        sb = new StringBuilder();
+        boolean isDot = false;
+        
+        for (char alpha : answer.toCharArray()) {
+            if (alpha == '.') {
+                if (isDot) {
+                    continue;
+                } else {
+                    isDot = true;
+                    sb.append(alpha);
+                }
+            } else {
+                sb.append(alpha);
+                isDot = false;
+            }
+        }
+        
+        if (sb.length() != 0) {
+            if (sb.charAt(0) == '.') {
+                sb.delete(0, 1);
+            }
+        }
+        if (sb.length() != 0) {
+            if (sb.charAt(sb.length() - 1) == '.') {
+                sb.delete(sb.length() - 1, sb.length());
+            }   
+        }
+        
+        if (sb.length() == 0) {
+            sb.append('a');
+        }
+        
+        if (sb.length() >= 16) {
+            sb.delete(15, sb.length());
+        }
+        
+        if (sb.charAt(sb.length() - 1) == '.') {
+            sb.delete(sb.length() - 1, sb.length());
+        }   
+        
+        if (sb.length() <= 2) {
+            while (sb.length() < 3) {
+                sb.append(sb.charAt(sb.length() - 1));
+            }
+        }
+        return sb.toString();
+    }
+}
