fc50e54a519d78b5c1a7fb1de52b0c692d4428a1
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-05-07 23:15:30 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,17 +3,16 @@ class Solution {
         int[] answer = new int[2];
         
         int total = brown + yellow;
-        
-        for (int i = 2; i <= total; i++) {
+        for (int i = 3; i < total; i++) {
             if (total % i == 0) {
-                if ((i - 2) * (total / i - 2) == yellow) {
-                    answer[0] = total / i;
-                    answer[1] = i;
-                    break;
+                int x = total / i;
+                int y = i;
+                if ((x - 2) * (y - 2) == yellow) {
+                    answer[0] = y;
+                    answer[1] = x;
                 }
             }
         }
-        
         return answer;
     }
 }
