168258326db49866c36ba2f0a0aa58a87fc0874d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-05-12 02:09:02 +0900

    [D1] Title: 신문 헤드라인, Time: 122 ms, Memory: 26,860 KB -BaekjoonHub

---
+++ SWEA/D1/2047. 신문 헤드라인/신문 헤드라인.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		String T=sc.next();
+        System.out.println(T.toUpperCase());
+	}
+}
