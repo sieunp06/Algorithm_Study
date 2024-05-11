f8953b8b71b481e3f6753c52e234128ade324fcb
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-05-12 02:24:39 +0900

    [D1] Title: 아주 간단한 계산기, Time: 131 ms, Memory: 25,960 KB -BaekjoonHub

---
+++ SWEA/D1/1938. 아주 간단한 계산기/아주 간단한 계산기.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(Math.round(a / b));
+	}
+}
