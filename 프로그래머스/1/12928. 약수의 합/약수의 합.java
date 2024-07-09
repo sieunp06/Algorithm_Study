3ed5b4d96b9769eee3b08f397f7c2f154a893176
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-09 15:09:01 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,9 +1,13 @@
-import java.util.stream.*;
-
 class Solution {
     public int solution(int n) {
-        return IntStream.rangeClosed(1, n)
-            .filter(number -> n % number == 0)
-            .sum();
+        int answer = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        
+        return answer;
     }
 }
