3808133538514a50954be1237c1f6784f6a9b868
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-09 13:39:56 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -1,9 +1,11 @@
-import java.util.stream.*;
-
 public class Solution {
     public int solution(int n) {
-        return String.valueOf(n).chars()
-            .map(number -> number - '0')
-            .sum();
+        int answer = 0;
+
+        for (char number: String.valueOf(n).toCharArray()) {
+            answer += number - '0';
+        }
+
+        return answer;
     }
 }
