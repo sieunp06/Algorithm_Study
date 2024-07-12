a44399ebf2cbc00571985cd3a2beee4b5f4864aa
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-12 13:21:34 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 22.21 ms, Memory: 87.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        String target = "";
+        for (int i = 1; i < food.length; i++) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer += i;
+            }
+        }
+        
+        for (int i = answer.length() - 1; i >= 0; i--) {
+            target += answer.charAt(i);
+        }
+        
+        return answer + '0' + target;
+    }
+}
