6d8180c4d6a466ce87f12bfc546893ddd9806f91
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 18:03:30 +0900

    [level 1] Title: 소수 만들기, Time: 18.09 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,30 +1,27 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
+        
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 for (int k = j + 1; k < nums.length; k++) {
-                    list.add(nums[i] + nums[j] + nums[k]);
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (isPrime(sum)) {
+                        answer++;
+                    }
                 }
-            }
-        }
-
-        for (int num : list) {
-            int number = 0;
-            for (int i = 2; i < num; i++) {
-                if (num % i == 0) {
-                    number++;
-                }
-            }
-            if (number == 0) {
-                answer++;
             }
         }
 
         return answer;
+    }
+    
+    private boolean isPrime(int num) {
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
