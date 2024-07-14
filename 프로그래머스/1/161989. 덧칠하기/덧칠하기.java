7c1fe61df24a1fabc7ededde85157c0e686b456b
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 18:31:09 +0900

    [level 1] Title: 덧칠하기, Time: 1.72 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,29 +1,14 @@
 class Solution {
-    // n : 구역 개수, m : 롤러의 길이
     public int solution(int n, int m, int[] section) {
         int answer = 0;
-        int[] paint = new int[n + 1];
+        int target = 0;
         
-        for (int secNum : section) {
-            paint[secNum] = 1;
-        }
-        
-        for (int i = 1; i < paint.length; i++) {
-            if (paint[i] == 1) {
-                if (i + m > n) {
-                    for (int j = i; j < paint.length; j++) {
-                        paint[j] = 0;
-                    }
-                    answer++;
-                    break;
-                } else {
-                    for (int j = i; j < i + m; j++) {
-                        paint[j] = 0;
-                    }
-                }
-                i += m - 1;
-                answer++;
+        for (int s: section) {
+            if (s <= target) {
+                continue;
             }
+            target = s + m - 1;
+            answer++;
         }
         
         return answer;
