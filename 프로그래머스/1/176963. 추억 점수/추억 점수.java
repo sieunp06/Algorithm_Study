2d5457be411eab0bd75b2b702fdd41ca1d879e01
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 17:04:25 +0900

    [level 1] Title: 추억 점수, Time: 2.20 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -1,22 +1,31 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
-    public List<Integer> solution(String[] name, int[] yearning, String[][] photo) {
-        List<Integer> answer = new ArrayList<>();
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        List<Integer> score = new ArrayList<>();
+        Map<String, Integer> yearningMap = new HashMap<>();
         
-        for (String[] person : photo) {
-            List<String> texts = new ArrayList<>();
-            for (int i = 0; i < person.length; i++) {
-                texts.add(person[i]);
-            }
-            int score = 0;
-            for (int i = 0; i < name.length; i++) {
-                if (texts.contains(name[i])) {
-                    score += yearning[i];
+        for (int i = 0; i < name.length; i++) {
+            yearningMap.put(name[i], yearning[i]);
+        }
+        
+        for (String[] ph: photo) {
+            int total = 0;
+            for (String na: ph) {
+                if (yearningMap.containsKey(na)) {
+                    total += yearningMap.get(na);
                 }
             }
-            answer.add(score);
+            score.add(total);
+        }
+        
+        int[] answer = new int[score.size()];
+        
+        for (int i = 0; i < score.size(); i++) {
+            answer[i] = score.get(i);
         }
         
         return answer;
