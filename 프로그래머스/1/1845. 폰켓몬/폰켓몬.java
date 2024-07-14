5a106cbe1825f82531b4f0a4fe887415e92587c9
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 17:24:56 +0900

    [level 1] Title: 폰켓몬, Time: 14.03 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,20 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        int target = nums.length / 2;
         
-        for (int num : nums) {
-            set.add(num);
-            
-            if (set.size() == nums.length / 2) {
-                break;
-            }
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        System.out.println(map);
         
-        return set.size();
+        if (map.size() < target) {
+            return map.size();
+        }
+        return target;
     }
 }
