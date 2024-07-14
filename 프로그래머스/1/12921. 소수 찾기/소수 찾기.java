94f03b67b143c541edfbd60e057e5f4d53e5f8aa
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 18:22:17 +0900

    [level 1] Title: 소수 찾기, Time: 213.30 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -2,24 +2,21 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int[] nums = new int[n + 1];
-        
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] == 1) {
-                continue;
-            }
-            
-            for (int j = 2 * i; j < nums.length; j += i) {
-                nums[j] = 1;
-            }
-        }
-        
-        for (int i = 2; i < nums.length; i++) {
-            if (nums[i] == 0) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
                 answer++;
             }
         }
         
         return answer;
+    }
+    
+    private boolean isPrime(int number) {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
