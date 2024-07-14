b52faea6cff14e74d9b7abde2b11025d6b41780f
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-14 18:18:18 +0900

    [level 1] Title: 과일 장수, Time: 184.88 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -5,17 +6,15 @@ import java.util.Collections;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        
         List<Integer> list = new ArrayList<>();
         for (int num : score) {
             list.add(num);
         }
-        Collections.sort(list, Collections.reverseOrder());
         
-        for (int i = m - 1; i < list.size(); i += m) {
+        Collections.sort(list, Collections.reverseOrder());
+        for (int i = m - 1; i < score.length; i += m) {
             answer += list.get(i) * m;
         }
-        
         return answer;
     }
 }
