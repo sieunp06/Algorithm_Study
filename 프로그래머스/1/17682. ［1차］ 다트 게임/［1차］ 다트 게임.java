04bae25ccd32bbd8db618b21e1dccf9b6a9321da
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-15 15:09:09 +0900

    [level 1] Title: [1차] 다트 게임, Time: 30.60 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -1,46 +1,51 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int solution(String dartResult) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        int idx = 0;
+        int[] scores = new int[3];
         
-        while (idx < dartResult.length() - 1) {
-            list.add(dartResult.charAt(idx) - '0');
-            idx++;
-            if (dartResult.charAt(idx) >= 48 && dartResult.charAt(idx) <= 57) {
-                String numst = String.valueOf(list.get(list.size() - 1)) + String.valueOf(dartResult.charAt(idx));
-                list.set(list.size() - 1, Integer.parseInt(numst));
-                idx++;
+        int chance = -1;
+        String score = "";
+        for (char alphabet: dartResult.toCharArray()) {
+            if (Character.isDigit(alphabet)) {
+                score += alphabet;
+                continue;
             }
-
-            if (dartResult.charAt(idx) == 'D') {
-                list.set(list.size() - 1, (int) Math.pow(list.get(list.size() - 1), 2));
-            } else if (dartResult.charAt(idx) == 'T') {
-                list.set(list.size() - 1, (int) Math.pow(list.get(list.size() - 1), 3));
-            } 
-            idx++;
-            if (idx < dartResult.length()) {
-                if (dartResult.charAt(idx) == '*') {
-                    if (list.size() > 1) {
-                        for (int i = list.size() - 2; i < list.size(); i++) {
-                            list.set(i, list.get(i) * 2);
-                        }   
-                    } else {
-                        list.set(0, list.get(0) * 2);
-                    }
-                    idx++;
-                } else if (dartResult.charAt(idx) == '#') {
-                    list.set(list.size() - 1, list.get(list.size() - 1) * -1);
-                    idx++;
+            if (alphabet == 'S') {
+                chance++;
+                scores[chance] = Integer.parseInt(score);
+                score = "";
+            }
+            if (alphabet == 'D') {
+                chance++;
+                scores[chance] = Integer.parseInt(score);
+                score = "";
+                scores[chance] = (int) Math.pow(scores[chance], 2);
+                continue;
+            }
+            if (alphabet == 'T') {
+                chance++;
+                scores[chance] = Integer.parseInt(score);
+                score = "";
+                scores[chance] = (int) Math.pow(scores[chance], 3);
+                continue;
+            }
+            if (alphabet == '*') {
+                scores[chance] = 2 * scores[chance];
+                if (chance > 0) {
+                    scores[chance - 1] = 2 * scores[chance - 1];
                 }
+            }
+            if (alphabet == '#') {
+                scores[chance] = -1 * scores[chance];
             }
         }
         
-        for (int num : list) {
-            answer += num;
+        for (int s: scores) {
+            answer += s;
         }
         
         return answer;
