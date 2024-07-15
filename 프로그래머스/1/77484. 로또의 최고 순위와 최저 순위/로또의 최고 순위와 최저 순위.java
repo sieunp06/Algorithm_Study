69c1df720e03645f538cfaddfee23193f32af7ea
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-15 15:22:05 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 4.48 ms, Memory: 93.5 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,46 +1,46 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.*;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
         int[] answer = new int[2];
-        List<Integer> list = new ArrayList<>();
+        List<Integer> win_nums_list = Arrays.stream(win_nums)
+            .boxed()
+            .collect(Collectors.toList());
         
-        for (int win_num : win_nums) {
-            list.add(win_num);
-        }
+        int numOfWin = Arrays.stream(lottos)
+            .filter(num -> win_nums_list.contains(num))
+            .boxed()
+            .collect(Collectors.toList()).size();
         
-        for (int lotto : lottos) {
-            if (list.contains(lotto)) {
-                answer[1]++;
-            }
-            if (lotto == 0) {
-                answer[0]++;
-            }
-        }
+        int numOfZero = Arrays.stream(lottos)
+            .filter(num -> num == 0)
+            .boxed()
+            .collect(Collectors.toList()).size();
         
-        if (answer[1] == 6) {
-            answer[0] = 6;
-        } else {
-            answer[0] += answer[1];
-        }
-        
-        for (int i = 0; i < answer.length; i++) {
-            if (answer[i] == 6) {
-                answer[i] = 1;
-            } else if (answer[i] == 5) {
-                answer[i] = 2;
-            } else if (answer[i] == 4) {
-                answer[i] = 3;
-            } else if (answer[i] == 3) {
-                answer[i] = 4;
-            } else if (answer[i] == 2) {
-                answer[i] = 5;
-            } else {
-                answer[i] = 6;
-            }
-        }
-        
+        answer[0] = giveAward(numOfWin + numOfZero);
+        answer[1] = giveAward(numOfWin);
         return answer;
+    }
+    
+    private int giveAward(int win) {
+        if (win == 6) {
+            return 1;
+        }
+        if (win == 5) {
+            return 2;
+        }
+        if (win == 4) {
+            return 3;
+        }
+        if (win == 3) {
+            return 4;
+        }
+        if (win == 2) {
+            return 5;
+        }
+        return 6;
     }
 }
