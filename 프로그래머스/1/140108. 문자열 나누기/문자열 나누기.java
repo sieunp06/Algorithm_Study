c166a262f127a0b350bda62beecfac374c0c24d0
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-15 15:35:39 +0900

    [level 1] Title: 문자열 나누기, Time: 0.99 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,40 +1,26 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
-        int idx = 1;
         
-        char alpha = s.charAt(0);
-        int same = 1; int diff = 0;
-        
-        while (idx < s.length()) {
-            if (s.charAt(idx) == alpha) {
-                same++;
-            } else {
-                diff++;
+        char x = s.charAt(0);
+        int numOfx = 1;
+        int numOfNotx = 0;
+        for (int i = 1; i < s.length(); i++) {
+            if (numOfx == numOfNotx) {
+                answer++;
+                x = s.charAt(i);
+                numOfx = 1;
+                numOfNotx = 0;
+                continue;
             }
-            
-            if (same == diff) {
-                list.add(idx);
-                idx++;
-                if (idx < s.length()) {
-                    alpha = s.charAt(idx);
-                    same = 1; diff = 0;   
-                } else {
-                    break;
-                }
+            if (x == s.charAt(i)) {
+                numOfx++;
+                continue;
             }
-            idx++;
+            if (x != s.charAt(i)) {
+                numOfNotx++;
+            }
         }
-        if (list.isEmpty()) {
-            return list.size() + 1;
-        }
-        if (list.get(list.size() - 1) != s.length() - 1) {
-            return list.size() + 1;
-        }
-        return list.size();
+        return answer + 1;
     }
 }
