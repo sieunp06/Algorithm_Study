25eaa31961f012e5903cd332253b1152346bd83e
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-07-27 17:37:09 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 35.00 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,19 +1,16 @@
 class Solution {
-    final String GOAL = "1";
     public int[] solution(String s) {
-        int round = 0;
-        int removeCount = 0;
+        int[] answer = {0, 0};
         
-        while (!s.equals(GOAL)) {
-            int len = s.replaceAll("0", "").length();
+        while (!s.equals("1")) {
+            int len = s.length();
+            s = s.replaceAll("0", "");
+            answer[1] += len - s.length();
+            s = Integer.toString(s.length(), 2);
+            answer[0]++;
             
-            round++;
-            removeCount += s.length() - len;
-            
-            s = Integer.toString(len, 2);
         }
         
-        int[] answer = {round, removeCount};
         return answer;
     }
 }
