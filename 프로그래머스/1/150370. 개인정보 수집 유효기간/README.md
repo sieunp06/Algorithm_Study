9126e1d8299dab1db0037698effd872fe727d7f8
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-06 13:04:54 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 6.53 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/README.md
@@ -1,10 +1,10 @@
 # [level 1] 개인정보 수집 유효기간 - 150370 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150370) 
 
 ### 성능 요약
 
-메모리: 69.7 MB, 시간: 2.88 ms
+메모리: 75 MB, 시간: 6.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 5일 4:35:44
+2024년 08월 06일 13:04:38
 
 ### 문제 설명
 
--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,60 +1,66 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.StringTokenizer;
+
+import java.util.Arrays;
 
 class Solution {
     public List<Integer> solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> termMap = new HashMap<>();
+        StringTokenizer st;
         
-        String[] today_split = today.split("[.]");
-        int today_year = Integer.parseInt(today_split[0]);
-        int today_month = Integer.parseInt(today_split[1]);
-        int today_day = Integer.parseInt(today_split[2]);
+        Map<String,Integer> map = new HashMap<>();
+        List<Integer> trash = new ArrayList<>();
         
         for (String term : terms) {
-            StringTokenizer st = new StringTokenizer(term);
-            termMap.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+            st = new StringTokenizer(term);
+            map.put(st.nextToken(), Integer.parseInt(st.nextToken()));
         }
         
-        for (int i = 0; i < privacies.length; i++) {
-            StringTokenizer st = new StringTokenizer(privacies[i]);
+        int idx = 1;
+        for (String privacy : privacies) {
+            st = new StringTokenizer(privacy);
+            String date = st.nextToken();
+            String term = st.nextToken();
             
-            String[] dates = st.nextToken().split("[.]");
+            String[] expiredDate = date.split("[.]");
+            int year = Integer.parseInt(expiredDate[0]);
+            int month = Integer.parseInt(expiredDate[1]) + map.get(term); 
+            int day = Integer.parseInt(expiredDate[2]) - 1;
             
-            int year = Integer.parseInt(dates[0]);
-            int month = Integer.parseInt(dates[1]) + termMap.get(st.nextToken());
-            int day = Integer.parseInt(dates[2]) - 1;
-            
-            if (day < 1) {
-                month--;
-                day += 28;
-            }
             while (month > 12) {
                 year++;
                 month -= 12;
             }
             
+            String[] todayDate = today.split("[.]");
+            int today_year = Integer.parseInt(todayDate[0]);
+            int today_month = Integer.parseInt(todayDate[1]);
+            int today_day = Integer.parseInt(todayDate[2]);
+            
             if (year < today_year) {
-                answer.add(i + 1);
-                continue;
-            } else if (year > today_year) {
-                continue;
-            }
-            if (month < today_month) {
-                answer.add(i + 1);
-                continue;
-            } else if (month > today_month) {
+                trash.add(idx);
+                idx++;
                 continue;
             }
-            if (day < today_day) {
-                answer.add(i + 1);
+            if (year == today_year) {
+                if (month < today_month) {
+                    trash.add(idx);
+                    idx++;
+                    continue;
+                } 
+                if (month == today_month) {
+                    if (day < today_day) {
+                        trash.add(idx);
+                        idx++;
+                        continue;
+                    }
+                }
             }
+            idx++;
         }
         
-        
-        return answer;
+        return trash;
     }
 }
