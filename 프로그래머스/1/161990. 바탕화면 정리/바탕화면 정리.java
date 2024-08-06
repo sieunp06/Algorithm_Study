5ca10578659efedd05b0840de6191ce828ed0c43
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-06 12:29:50 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.26 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        
-        int min_x = Integer.MAX_VALUE; int max_x = Integer.MIN_VALUE;
-        int min_y = Integer.MAX_VALUE; int max_y = Integer.MIN_VALUE;
+        int[] answer = {Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE};
         
         for (int i = 0; i < wallpaper.length; i++) {
-            for (int j = 0; j < wallpaper[i].length(); j++) {
-                if (wallpaper[i].charAt(j) == '#') {
-                    if (min_x > j) {
-                        min_x = j;
-                    }
-                    if (min_y > i) {
-                        min_y = i;
-                    }
-                    if (max_x < j) {
-                        max_x = j;
-                    }
-                    if (max_y < i) {
-                        max_y = i;
-                    }
+            String wall = wallpaper[i];
+            for (int j = 0; j < wall.length(); j++) {
+                if (wall.charAt(j) == '#') {
+                    int y = i; int x = j;
+                    if (y <= answer[0]) answer[0] = y;
+                    if (x <= answer[1]) answer[1] = x;
+                    if (y >= answer[2]) answer[2] = y + 1;
+                    if (x >= answer[3]) answer[3] = x + 1;
                 }
             }
         }
-        
-        answer[0] = min_y; answer[1] = min_x;
-        answer[2] = max_y + 1; answer[3] = max_x + 1;
         
         return answer;
     }
