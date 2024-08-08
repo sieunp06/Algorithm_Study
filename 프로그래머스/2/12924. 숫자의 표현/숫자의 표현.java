daea6a7e163901720b2a2f807f85f4e9167e2e70
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-08 14:23:12 +0000

    [level 2] Title: 숫자의 표현, Time: 0.55 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -2,12 +2,17 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int start = 1; int end = 1;
-        int total = 1;
-        while (end <= n) {
+        if (n == 1) {
+            return 1;
+        }
+        
+        int start = 1; int end = 2;
+        int total = start + end;
+        
+        while (end < n) {
             if (total == n) {
                 answer++;
-                total -= start; 
+                total -= start;
                 start++; end++;
                 total += end;
             } else if (total < n) {
@@ -19,6 +24,6 @@ class Solution {
             }
         }
         
-        return answer;
+        return answer + 1;
     }
 }
