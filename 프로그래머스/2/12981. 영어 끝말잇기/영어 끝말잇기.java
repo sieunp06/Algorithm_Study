b9c61016b67b95d072e5ea209fefc0af87830f76
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-16 22:19:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,41 +1,30 @@
 import java.util.Set;
 import java.util.HashSet;
 
-
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        int[] saying = new int[n + 1];
-        
-        Set<String> set = new HashSet<>();
-        
-        String word_st = words[0];
-        set.add(words[0]);
-        saying[1]++;
-        int idx = 2;
-        
-        for (int i = 1; i < words.length; i++) {            
-            int len = set.size();
-            set.add(words[i]);
-            saying[idx]++;
 
-            if (word_st.charAt(word_st.length() - 1) != words[i].charAt(0)) {
-                answer[0] = idx;
-                answer[1] = saying[idx];
-                break;
+        Set<String> wordList = new HashSet<>();
+         
+        char lastAlphabet = words[0].charAt(words[0].length() - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (wordList.contains(words[i])) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                return answer;
             }
-
-            if (len == set.size()) {
-                answer[0] = idx;
-                answer[1] = saying[idx];
-                break;
+            wordList.add(words[i]);
+            if (i == 0) {
+                lastAlphabet = words[i].charAt(words[i].length() - 1);
+                continue;
             }
-            
-            idx++;
-            word_st = words[i];
-            if (idx > n) {
-                idx = 1;
+            if (words[i].charAt(0) != lastAlphabet) {
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
+                return answer;
             }
+            lastAlphabet = words[i].charAt(words[i].length() - 1);
         }
 
         return answer;
