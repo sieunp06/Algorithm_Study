26643686e469d1260cd9e961a86aa4a2ee2b986a
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-16 20:53:50 +0900

    [level 2] Title: 구명보트, Time: 15.42 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,26 +5,22 @@ class Solution {
         int answer = 0;
         
         Arrays.sort(people);
-        
         int start = 0; int end = people.length - 1;
-        int total = people[start] + people[end];
         
         while (start <= end) {
+            int total = people[start] + people[end];
             if (start == end) {
                 answer++;
                 break;
-            } else if (total <= limit) {
-                answer++;
+            }
+            if (total <= limit) {
                 start++; end--;
-                total = people[start] + people[end];
             } else {
-                answer++;
-                total -= people[end];
                 end--;
-                total += people[end];
-            } 
+            }
+            answer++;
         }
-        
+
         return answer;
     }
 }
