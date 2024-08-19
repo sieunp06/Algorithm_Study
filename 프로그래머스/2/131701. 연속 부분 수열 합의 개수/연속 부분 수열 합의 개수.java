8b0191cb6628d5f327412c21f05111e1f40a10b9
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-08-19 21:44:16 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 234.66 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,43 +3,20 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        
         Set<Integer> set = new HashSet<>();
-        int total = 0;
-        for (int element: elements) {
-            total += element;
-            set.add(element);
-        }
-        set.add(total);
+        int[] doubleElements = new int[elements.length * 2];
         
-        int[] expendedElements = new int[elements.length * 2];
+        for (int i = 0; i < doubleElements.length; i++) {
+            doubleElements[i] = elements[i % elements.length];
+        }
         
         for (int i = 0; i < elements.length; i++) {
-            expendedElements[i] = elements[i];
-        }
-        
-        for (int i = elements.length; i < expendedElements.length; i++) {
-            expendedElements[i] = elements[i - elements.length];
-        }
-        
-        for (int i = 2; i < elements.length; i++) {
-            int start = 0; int end = start + i;
-            int total2 = 0;
-            
-            for (int j = start; j < end; j++) {
-                total2 += expendedElements[j];
-            }
-            set.add(total2);            
-            while (start < elements.length - 1) {
-                total2 -= expendedElements[start];
-                start++;
-                total2 += expendedElements[end];
-                end++;
-                set.add(total2);
+            int total = 0;
+            for (int j = i; j < elements.length + i; j++) {
+                total += doubleElements[j];
+                set.add(total);
             }
         }
-        
         return set.size();
     }
 }
