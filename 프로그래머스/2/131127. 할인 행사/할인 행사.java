87ec6e1268fa3098f72080805f934b79f6208751
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-09-04 23:25:03 +0900

    [level 2] Title: 할인 행사, Time: 29.88 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -5,24 +5,22 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        Map<String, Integer> map = new HashMap<>();
-        
+        Map<String, Integer> wantMap = new HashMap<>();
         for (int i = 0; i < want.length; i++) {
-            map.put(want[i], number[i]);
+            wantMap.put(want[i], number[i]);
         }
         
-        int start = 0; int end = start + 9;
-        
-        for (int i = start; i <= end; i++) {
-            if (map.containsKey(discount[i])) {
-                map.put(discount[i], map.get(discount[i]) - 1);   
+        for (int i = 0; i < 10; i++) {
+            if (wantMap.containsKey(discount[i])) {
+                wantMap.put(discount[i], wantMap.get(discount[i]) - 1);
             }
         }
         
-        while (end < discount.length - 1) {
+        int start = 0; int end = start + 10;
+        while (end < discount.length) {
             boolean flag = true;
-            for (String text : want) {
-                if (map.get(text) > 0) {
+            for (String name : want) {
+                if (wantMap.get(name) > 0) {
                     flag = false;
                     break;
                 }
@@ -30,23 +28,23 @@ class Solution {
             if (flag) {
                 answer++;
             }
-            if (map.containsKey(discount[start])) {
-                map.put(discount[start], map.get(discount[start]) + 1);
+            if (wantMap.containsKey(discount[start])) {
+                wantMap.put(discount[start], wantMap.get(discount[start]) + 1);
+            }
+            if (wantMap.containsKey(discount[end])) {
+                wantMap.put(discount[end], wantMap.get(discount[end]) - 1);
             }
             start++; end++;
-            if (map.containsKey(discount[end])) {
-                map.put(discount[end], map.get(discount[end]) - 1);
-            }
         }
         
-        boolean flag2 = true;
-        for (String text : want) {
-            if (map.get(text) > 0) {
-                flag2 = false;
+        boolean flag = true;
+        for (String name : want) {
+            if (wantMap.get(name) > 0) {
+                flag = false;
                 break;
             }
         }
-        if (flag2) {
+        if (flag) {
             answer++;
         }
         
