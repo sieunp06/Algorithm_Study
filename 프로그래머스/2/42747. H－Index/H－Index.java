2df024a8286795b41f45e5f094a08efb9d125fdb
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-09-09 23:35:05 +0900

    [level 2] Title: H-Index, Time: 1.58 ms, Memory: 65.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -3,13 +3,12 @@ import java.util.Arrays;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        
         Arrays.sort(citations);
         
         for (int i = 0; i < citations.length; i++) {
-            int h = citations.length - i;
-            
-            if (citations[i] >= h) {
-                answer = h;
+            if (citations[i] >= citations.length - i) {
+                answer = citations.length - i;
                 break;
             }
         }
