4673d13871f908593cd13ad523c7a5d0ef711f73
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-10-08 16:59:23 +0900

    [level 2] Title: 전화번호 목록, Time: 314.00 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,29 +1,13 @@
 import java.util.Arrays;
-import java.util.Set;
-import java.util.HashSet;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        
-        Set<String> set = new HashSet<>();
-        
-        for (String phoneNum: phone_book) {
-            set.add(phoneNum);
-        }
-        
-        if (phone_book.length != set.size()) {
-            return false;
-        }
-        
         Arrays.sort(phone_book);
-        
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            if (phone_book[i + 1].startsWith(phone_book[i])) {
-                    return false;
-                }
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i - 1])) {
+                return false;
+            }
         }
-        
-        return answer;
+        return true;
     }
 }
