c6e7e71b87e356a0b3edbdfc56041b117bd57580
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-10-08 17:17:41 +0900

    [level 2] Title: 의상, Time: 0.11 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,18 +3,15 @@ import java.util.HashMap;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        
         Map<String, Integer> map = new HashMap<>();
-        
         for (String[] cloth : clothes) {
             map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1);
         }
         
+        int answer = 1;
         for (String kind : map.keySet()) {
             answer *= map.get(kind) + 1;
         }
-        
         return answer - 1;
     }
 }
