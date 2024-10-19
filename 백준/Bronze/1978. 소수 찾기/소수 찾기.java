701f1416f006cc43ed101f6d9c22d6d905231989
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-10-19 17:44:39 +0900

    [Bronze II] Title: 소수 찾기, Time: 116 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,32 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int answer = 0;
-
-        int N = Integer.parseInt(bf.readLine());
-
-        st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-
-            if (num != 1) {
-                boolean isSosoo = true;
-                for (int j = 2; j < num; j++) {
-                    if (num % j == 0)
-                        isSosoo = false;
+            if (num == 1) {
+                continue;
+            }
+            boolean flag = true;
+            for (int j = 2; j < num; j++) {
+                if (num % j == 0) {
+                    flag = false;
+                    break;
                 }
-                if (isSosoo)
-                    answer++;
+            }
+            if (flag) {
+                answer++;
             }
         }
-
         System.out.println(answer);
     }
 }
