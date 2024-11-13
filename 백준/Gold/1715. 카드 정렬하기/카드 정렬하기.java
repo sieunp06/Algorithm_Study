bc8bc88aff708b363caa89de975f480067578244
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-11-13 18:33:45 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 324 ms, Memory: 25332 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -1,30 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine());
 
-        int N = Integer.parseInt(bf.readLine());
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-
-        for (int i = 0; i < N; i++)
-            q.add(Integer.parseInt(bf.readLine()));
-
-        int data1 = 0;
-        int data2 = 0;
-        int total = 0;
-
-        while (q.size() != 1) {
-            data1 = q.remove();
-            data2 = q.remove();
-            
-            total += data1 + data2;
-
-            q.add(data1 + data2);
+        PriorityQueue<Integer> cards = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+             cards.add(Integer.parseInt(bufferedReader.readLine()));
         }
+
+        int total = 0;
+        while (cards.size() > 1) {
+            int num1 = cards.poll();
+            int num2 = cards.poll();
+            total += num1 + num2;
+            cards.add(num1 + num2);
+    }
+
         System.out.println(total);
     }
 }
