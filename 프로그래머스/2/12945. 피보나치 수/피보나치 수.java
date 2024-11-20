b97fadf3e483df2b85e808fb28c4f95ce62a5f91
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-11-20 13:02:59 +0900

    [level 2] Title: 피보나치 수, Time: 2.28 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,15 +1,14 @@
-import java.util.List;
-import java.util.ArrayList;
-
 class Solution {
     public int solution(int n) {
-        List<Integer> num = new ArrayList<>();
-        num.add(0);
-        num.add(1);
+        int[] fibos = new int[100001];
         
-        for (int i = 2; i <= n; i++) {
-            num.add((num.get(i - 2) + num.get(i - 1)) % 1234567); 
+        fibos[0] = 0;
+        fibos[1] = 1; fibos[2] = 1;
+        
+        for (int i = 3; i <= n; i++) {
+            fibos[i] = (fibos[i - 2] + fibos[i - 1]) % 1234567;
         }
-        return num.get(n);
+
+        return fibos[n];
     }
 }
