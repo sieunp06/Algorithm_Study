fc4644486b102960bc15007b49fb2b227e2fe6fc
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2024-11-22 23:48:35 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.28 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,30 +1,41 @@
 import java.util.Set;
 import java.util.HashSet;
 
+
 class Solution {
     public int[] solution(int n, String[] words) {
         int[] answer = new int[2];
+        int[] saying = new int[n + 1];
+        
+        Set<String> set = new HashSet<>();
+        
+        String word_st = words[0];
+        set.add(words[0]);
+        saying[1]++;
+        int idx = 2;
+        
+        for (int i = 1; i < words.length; i++) {            
+            int len = set.size();
+            set.add(words[i]);
+            saying[idx]++;
 
-        Set<String> wordList = new HashSet<>();
-         
-        char lastAlphabet = words[0].charAt(words[0].length() - 1);
-        for (int i = 0; i < words.length; i++) {
-            if (wordList.contains(words[i])) {
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-                return answer;
+            if (word_st.charAt(word_st.length() - 1) != words[i].charAt(0)) {
+                answer[0] = idx;
+                answer[1] = saying[idx];
+                break;
             }
-            wordList.add(words[i]);
-            if (i == 0) {
-                lastAlphabet = words[i].charAt(words[i].length() - 1);
-                continue;
+
+            if (len == set.size()) {
+                answer[0] = idx;
+                answer[1] = saying[idx];
+                break;
             }
-            if (words[i].charAt(0) != lastAlphabet) {
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-                return answer;
+            
+            idx++;
+            word_st = words[i];
+            if (idx > n) {
+                idx = 1;
             }
-            lastAlphabet = words[i].charAt(words[i].length() - 1);
         }
 
         return answer;
