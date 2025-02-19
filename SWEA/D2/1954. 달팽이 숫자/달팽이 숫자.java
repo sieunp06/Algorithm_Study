e797f85a5535a41898224de19221225520a3ba8d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-02-19 11:58:47 +0900

    [D2] Title: 달팽이 숫자, Time: 92 ms, Memory: 24,704 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,35 +1,61 @@
-import java.util.Scanner;
-import java.io.FileInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
-class Solution {
-    static int[] dx = { 0, 1, 0, -1 };
-    static int[] dy = { 1, 0, -1, 0 };
-	public static void main(String args[]) throws Exception {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
+public class Solution {
+    private static int T;   // 테스트케이스 개수
 
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = sc.nextInt();
-            int[][] nums = new int[N][N];
-            
-            int dir = 0; 
-            int x = 0; int y = 0;
-            for (int i = 1; i <= N * N; i++) {
-            	nums[x][y] = i;
-                if (x + dx[dir] >= N || x + dx[dir] < 0 || y + dy[dir] >= N || y + dy[dir] < 0 || nums[x + dx[dir]][y + dy[dir]] != 0) {
-                	dir = (dir + 1) % 4;
-                }
-                x += dx[dir];
-                y += dy[dir];
-            }
-            
-            System.out.println("#" + test_case);
+    private static int N;
+    private static int[][] boards;
+    private static boolean[][] visited;
+    private static int idx = 0;
+
+    // 우하좌상
+    private static final int[] dr = {0, 1, 0, -1};
+    private static final int[] dc = {1, 0, -1, 0};
+
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder stringBuilder = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(bufferedReader.readLine());
+        for (int testCase = 1; testCase <= T; testCase++) {
+            N = Integer.parseInt(bufferedReader.readLine());
+            boards = new int[N][N];
+            visited = new boolean[N][N];
+            fillNumbers();
+
+            stringBuilder.append("#").append(testCase).append("\n");
             for (int i = 0; i < N; i++) {
-            	for (int num : nums[i]) {
-                	System.out.print(num + " ");
+                for (int j = 0; j < N; j++) {
+                    stringBuilder.append(boards[i][j]).append(" ");
                 }
-                System.out.println();
+                stringBuilder.append("\n");
             }
-		}
-	}
+        }
+        System.out.print(stringBuilder);
+    }
+
+    private static void fillNumbers() {
+        int num = 1;
+        int r = 0; int c = 0;
+        boards[r][c] = num++;
+        visited[r][c] = true;
+
+        while(num <= N * N) {
+            r += dr[idx % 4];
+            c += dc[idx % 4];
+
+            if (r < 0 || r >= N || c < 0 || c >= N || visited[r][c]) {
+                r -= dr[idx % 4];
+                c -= dc[idx % 4];
+                idx++;
+                continue;
+            }
+            visited[r][c] = true;
+            boards[r][c] = num;
+            num++;
+        }
+    }
 }
