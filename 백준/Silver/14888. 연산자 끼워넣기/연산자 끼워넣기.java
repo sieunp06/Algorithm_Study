59ad7aabb0451b8f75ef3e6879ec1f1dae845a75
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-03-25 00:00:34 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 116 ms, Memory: 15868 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,40 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
-    static int[] numbers;
-    static int[] types;
+    private static int N;
+    private static int max = Integer.MIN_VALUE;
+    private static int min = Integer.MAX_VALUE;
+    private static int[] numbers;
+    private static int[] types;
 
-    public static void main(String[] arg) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static final StringBuilder stringBuilder = new StringBuilder();
+    private static StringTokenizer stringTokenizer;
 
-        N = Integer.parseInt(br.readLine());
-        numbers = new int[N];
-        types = new int[4];
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
-        }
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < types.length; i++) {
-            types[i] = Integer.parseInt(st.nextToken());
-        }
-
-        DFS(1, numbers[0]);
+    public static void main(String[] args) throws IOException {
+        init();
+        backtracking(1, numbers[0]);
 
         System.out.println(max);
         System.out.println(min);
     }
 
-    private static void DFS(int depth, int total) {
+    private static void init() throws IOException {
+        N = Integer.parseInt(bufferedReader.readLine());
+        numbers = new int[N];
+        types = new int[4];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < types.length; i++) {
+            types[i] = Integer.parseInt(stringTokenizer.nextToken());
+        }
+    }
+
+    private static void backtracking(int depth, int total) {
         if (depth == N) {
             max = Math.max(max, total);
             min = Math.min(min, total);
@@ -46,10 +50,10 @@ public class Main {
                 types[i]--;
 
                 switch (i) {
-                    case 0: DFS(depth + 1, total + numbers[depth]); break;
-                    case 1: DFS(depth + 1, total - numbers[depth]); break;
-                    case 2: DFS(depth + 1, total * numbers[depth]); break;
-                    case 3: DFS(depth + 1, total / numbers[depth]); break;
+                    case 0: backtracking(depth + 1, total + numbers[depth]); break;
+                    case 1: backtracking(depth + 1, total - numbers[depth]); break;
+                    case 2: backtracking(depth + 1, total * numbers[depth]); break;
+                    case 3: backtracking(depth + 1, total / numbers[depth]); break;
                 }
                 types[i]++;
             }
