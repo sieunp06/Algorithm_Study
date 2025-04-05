6c8a2bc76c25cfbe2ebd3db48e572b6204eefe33
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-04-05 14:39:25 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 157 ms, Memory: 34,884 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -5,40 +5,43 @@ public class Solution {
     static int N;
     static int[] x, y;
     static double E;
+    static boolean[] visited;
+    static double[] minEdge;
 
-    static List<Island> edges;
-    static int[] parents;
-
-    static class Island {
-        int from, to;
-        long weight;
-
-        Island(int from, int to, long weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
-        }
-    }
-
-    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static final StringBuilder stringBuilder = new StringBuilder();
     static StringTokenizer stringTokenizer;
+    static class Node implements Comparable<Node> {
+        int idx;
+        double cost;
+
+        public Node(int idx, double cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return Double.compare(this.cost, o.cost);
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(bufferedReader.readLine());
 
-        for (int tc = 1; tc <= T; tc++) {
+        for (int testCase = 1; testCase <= T; testCase++) {
             init();
-            stringBuilder.append("#").append(tc).append(" ").append(calculate()).append("\n");
+            stringBuilder.append("#").append(testCase).append(" ").append(Math.round(prim())).append("\n");
         }
-
         System.out.println(stringBuilder);
     }
 
     static void init() throws IOException {
-        N = Integer.parseInt(bufferedReader.readLine());    // 섬의 개수
+        N = Integer.parseInt(bufferedReader.readLine());
         x = new int[N];
         y = new int[N];
+        visited = new boolean[N];
+        minEdge = new double[N];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < N; i++) {
@@ -51,52 +54,45 @@ public class Solution {
         }
 
         E = Double.parseDouble(bufferedReader.readLine());
-
-        edges = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                long dx = x[i] - x[j];
-                long dy = y[i] - y[j];
-
-                edges.add(new Island(i, j, dx * dx + dy * dy));
-            }
-        }
-
-        parents = new int[N];
-        for (int i = 0; i < N; i++) {
-            parents[i] = i;
-        }
     }
 
-    static long calculate() {
-        edges.sort(Comparator.comparingLong(e -> e.weight));
+    static double prim() {
+        Arrays.fill(minEdge, Double.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        minEdge[0] = 0;
+        pq.add(new Node(0, 0));
 
+        double result = 0;
         int count = 0;
-        long totalCost = 0;
 
-        for (Island i : edges) {
-            if (union(i.from, i.to)) {
-                totalCost += i.weight;
-                count++;
-                if (count == N - 1) {
-                    break;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
+
+            if (visited[now.idx]) continue;
+
+            visited[now.idx] = true;
+            result += now.cost;
+            count++;
+
+            if (count == N) break;
+
+            for (int i = 0; i < N; i++) {
+                if (!visited[i]) {
+                    double dist = getDist(now.idx, i);
+                    if (minEdge[i] > dist) {
+                        minEdge[i] = dist;
+                        pq.add(new Node(i, dist));
+                    }
                 }
             }
         }
-        return Math.round(totalCost * E);
+
+        return result;
     }
 
-    static boolean union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA == rootB) return false;
-        parents[rootB] = rootA;
-        return true;
-    }
-
-    static int find(int a) {
-        if (parents[a] != a)
-            parents[a] = find(parents[a]);
-        return parents[a];
+    static double getDist(int a, int b) {
+        long dx = x[a] - x[b];
+        long dy = y[a] - y[b];
+        return (dx * dx + dy * dy) * E;
     }
 }
