5ac667890e988d80a85de700e0db9792534a5794
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-04-21 17:44:48 +0900

    [Gold V] Title: AC, Time: 668 ms, Memory: 94856 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,76 +1,87 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(bf.readLine());
-		
-		while (T --> 0) {
-			boolean check = true;
-			boolean isRev = false;
-			
-			LinkedList<Integer> dq = new LinkedList<>(); 
-			
-			String command = bf.readLine();
-			int n = Integer.parseInt(bf.readLine());
-			String arr_text = bf.readLine();
-			arr_text = arr_text.substring(1, arr_text.length() - 1);
-			
-			StringTokenizer st = new StringTokenizer(arr_text, ",");
-			for (int i = 0; i < n; i++)
-				dq.offer(Integer.parseInt(st.nextToken()));
+    static int T, n;
+    static String p, input;
+    static Deque<Integer> numbers;
 
-			for (int i = 0; i < command.length(); i++) {
-				char t = command.charAt(i);
-				
-				switch(t) {
-					case 'R':
-						if (isRev) 
-							isRev = false;
-						else isRev = true;
-						break;
-					case 'D':
-						if (dq.isEmpty() && check) {
-							sb.append("error").append('\n');
-							check = false;
-						} 
-						if (isRev)
-							dq.pollLast();
-                        else  
-							dq.poll();
-						break;
-				}
-			}
+    static boolean order;
+    static boolean isError;
 
-			if (check) {
-				sb.append('[');
-				int leng = dq.size();
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-				if (isRev) {
-					for (int i = 0; i < leng; i++) {
-						if (dq.size() > 1) 
-							sb.append(dq.pollLast()).append(',');
-						else 
-							sb.append(dq.pollLast());
+    public static void main(String[] args) throws IOException {
+        T = Integer.parseInt(bufferedReader.readLine());
+        while (T-- > 0) {
+            init();
+            precess();
+            printResult();
+        }
+        System.out.println(stringBuilder);
+    }
 
-					}
-				} else {
-					for (int i = 0; i < leng; i++) {
-						if (dq.size() > 1)
-							sb.append(dq.poll()).append(',');
-						else sb.append(dq.poll());
-					}
-				}
-				sb.append(']');
-				sb.append('\n');
-			}
-		}
-		System.out.print(sb);
-	}
+    static void init() throws IOException {
+        p = bufferedReader.readLine();
+        n = Integer.parseInt(bufferedReader.readLine());
+
+        numbers = new ArrayDeque<>();
+        order = true;
+        isError = false;
+
+        input = bufferedReader.readLine();
+        input = input.substring(1, input.length() - 1);
+        String[] inputs = input.split(",");
+
+        for (int i = 0; i < n; i++) {
+            numbers.add(Integer.parseInt(inputs[i]));
+        }
+    }
+
+    static void precess() {
+        for (char alpha : p.toCharArray()) {
+            if (alpha == 'R') { // 배열의 순서 뒤집기
+                order = !order;
+            }
+            if (alpha == 'D') { // 첫 번째 수 버리기
+                if (numbers.isEmpty()) {
+                    isError = true;
+                    break;
+                }
+                if (order) {    // 정방향
+                    numbers.pollFirst();
+                } else {
+                    numbers.pollLast();
+                }
+            }
+        }
+    }
+
+    static void printResult() {
+        if (isError) {
+            stringBuilder.append("error").append("\n");
+        } else {
+            stringBuilder.append("[");
+            while (!numbers.isEmpty()) {
+                if (order) {
+                    if (numbers.size() == 1) {
+                        stringBuilder.append(numbers.pollFirst());
+                    } else {
+                        stringBuilder.append(numbers.pollFirst()).append(",");
+                    }
+                } else {
+                    if (numbers.size() == 1) {
+                        stringBuilder.append(numbers.pollLast());
+                    } else {
+                        stringBuilder.append(numbers.pollLast()).append(",");
+                    }
+                }
+            }
+            stringBuilder.append("]").append("\n");
+        }
+    }
 }
