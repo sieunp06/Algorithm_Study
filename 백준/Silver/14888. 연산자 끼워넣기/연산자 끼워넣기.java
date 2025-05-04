6231a90f529ef0eace2eeda38d02b31ce936c399
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-05-04 15:03:42 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 108 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,31 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    private static int N;
-    private static int max = Integer.MIN_VALUE;
-    private static int min = Integer.MAX_VALUE;
-    private static int[] numbers;
-    private static int[] types;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private static final StringBuilder stringBuilder = new StringBuilder();
-    private static StringTokenizer stringTokenizer;
+    static int N;
+    static int[] numbers, sign;
+    static int[] answer = {Integer.MIN_VALUE, Integer.MAX_VALUE};
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         init();
         backtracking(1, numbers[0]);
-
-        System.out.println(max);
-        System.out.println(min);
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
     }
 
-    private static void init() throws IOException {
+    static void init() throws IOException {
         N = Integer.parseInt(bufferedReader.readLine());
         numbers = new int[N];
-        types = new int[4];
+        sign = new int[4];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < N; i++) {
@@ -33,29 +30,28 @@ public class Main {
         }
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < types.length; i++) {
-            types[i] = Integer.parseInt(stringTokenizer.nextToken());
+        for (int i = 0; i < 4; i++) {
+            sign[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
     }
 
-    private static void backtracking(int depth, int total) {
+    static void backtracking(int depth, int total) {
         if (depth == N) {
-            max = Math.max(max, total);
-            min = Math.min(min, total);
+            answer[0] = Math.max(answer[0], total);
+            answer[1] = Math.min(answer[1], total);
             return;
         }
 
         for (int i = 0; i < 4; i++) {
-            if (types[i] > 0) {
-                types[i]--;
-
+            if (sign[i] > 0) {
+                sign[i]--;
                 switch (i) {
                     case 0: backtracking(depth + 1, total + numbers[depth]); break;
                     case 1: backtracking(depth + 1, total - numbers[depth]); break;
                     case 2: backtracking(depth + 1, total * numbers[depth]); break;
                     case 3: backtracking(depth + 1, total / numbers[depth]); break;
                 }
-                types[i]++;
+                sign[i]++;
             }
         }
     }
