07b392918c72fe9929ebf651b695565c74f10083
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-05-05 12:05:30 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 248 ms, Memory: 26036 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    static int[][][] dp = new int[21][21][21];
+
+    public static void main(String[] args) throws IOException {
+        while (true) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int a = Integer.parseInt(stringTokenizer.nextToken());
+            int b = Integer.parseInt(stringTokenizer.nextToken());
+            int c = Integer.parseInt(stringTokenizer.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) break;
+
+            int result = w(a, b, c);
+            stringBuilder.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(result).append("\n");
+        }
+
+        System.out.print(stringBuilder);
+    }
+
+    static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) return 1;
+        if (a > 20 || b > 20 || c > 20) return w(20, 20, 20);
+
+        if (dp[a][b][c] != 0) return dp[a][b][c];
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+
+        return dp[a][b][c];
+    }
+}
