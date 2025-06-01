3f5c2cdedf6f04288b82a6c0ffe5d344873094ab
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-01 22:40:48 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 172 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,22 +1,16 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int bytes = Integer.parseInt(bf.readLine());
-        StringBuilder sb = new StringBuilder();
-        
-        bytes -= 4;
-        while (bytes != 0) {
-            sb.append("long ");
-            bytes -= 4;
-        }
-        
-        sb.append("long int");
-        
-        System.out.println(sb);
-    }
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		long N = input.nextLong();
+		if (N % 4 == 0) {
+			for(int i = 1; i<= N/4; i++) {
+				System.out.print("long ");
+			}
+		}
+		System.out.println("int");
+	}
+
 }
