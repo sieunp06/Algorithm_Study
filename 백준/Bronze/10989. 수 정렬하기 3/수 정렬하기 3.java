f8b08acda6fde57a7e173729d179b1e69b0c3b16
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-03 03:06:02 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1580 ms, Memory: 336644 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,28 +1,26 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        int[] numbers = new int[N];
-        
-        for (int i = 0; i < N; i++)
-            numbers[i] = Integer.parseInt(bf.readLine());
-        
-        Arrays.sort(numbers);
-        
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(bufferedReader.readLine());
+        int[] count = new int[10001];
+
         for (int i = 0; i < N; i++) {
-            bw.write(numbers[i] + "\n");
+            int num = Integer.parseInt(bufferedReader.readLine());
+            count[num]++;
         }
-        
-        bw.flush();
-        bw.close();
+
+        for (int i = 1; i <= 10000; i++) {
+            while (count[i]-- > 0) {
+                stringBuilder.append(i).append("\n");
+            }
+        }
+
+        System.out.print(stringBuilder);
     }
 }
