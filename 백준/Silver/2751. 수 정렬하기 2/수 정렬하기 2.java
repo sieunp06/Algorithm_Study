5bff1a653d42329b93e5188826fc467d18ed7546
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-03 22:38:55 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1324 ms, Memory: 129100 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,24 +1,25 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(bf.readLine());
-        ArrayList<Integer> numbers = new ArrayList<>();
-        
-        for (int i = 0; i < N; i++)
-        	numbers.add(Integer.parseInt(bf.readLine()));
-        
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            numbers.add(Integer.parseInt(bufferedReader.readLine()));
+        }
+
         Collections.sort(numbers);
-        
-        for (int x : numbers)
-            sb.append(x).append('\n');
-        System.out.println(sb);
+
+        for (int num : numbers) {
+            stringBuilder.append(num).append("\n");
+        }
+        System.out.println(stringBuilder);
     }
 }
