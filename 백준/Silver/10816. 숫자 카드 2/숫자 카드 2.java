47a23d2faf8ec6c03c50a3f2db273034e1e4c436
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-04 23:57:33 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 996 ms, Memory: 173456 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,69 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N, M;
-    private static int[] cards, target;
-
-    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private static final StringBuilder stringBuilder = new StringBuilder();
-    private static StringTokenizer stringTokenizer;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
     public static void main(String[] args) throws IOException {
-        init();
-        for (int num : target) {
-            stringBuilder.append(search_down(num) - search_up(num)).append(" ");
-        }
-        System.out.println(stringBuilder);
-    }
+        int N = Integer.parseInt(bufferedReader.readLine());
+        Map<Integer, Integer> map = new HashMap<>();
 
-    private static void init() throws IOException {
-        N = Integer.parseInt(bufferedReader.readLine());
-        cards = new int[N];
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < N; i++) {
-            cards[i] = Integer.parseInt(stringTokenizer.nextToken());
+            int num = Integer.parseInt(stringTokenizer.nextToken());
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        Arrays.sort(cards);
 
-        M = Integer.parseInt(bufferedReader.readLine());
-        target = new int[M];
+        int M = Integer.parseInt(bufferedReader.readLine());
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < M; i++) {
-            target[i] = Integer.parseInt(stringTokenizer.nextToken());
+            stringBuilder.append(map.getOrDefault(Integer.parseInt(stringTokenizer.nextToken()), 0)).append(" ");
         }
-    }
-
-    private static int search_down(int num) {
-        int left = 0; int right = N;
-        int mid;
-
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (num < cards[mid]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
-    }
-
-    private static int search_up(int num) {
-        int left = 0; int right = N;
-        int mid;
-
-        while (left < right) {
-            mid = (left + right) / 2;
-            if (num <= cards[mid]) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return left;
+        System.out.println(stringBuilder);
     }
 }
