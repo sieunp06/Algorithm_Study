47dd659af5031afd96074de4a6af692a6f88e2cd
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-04 23:51:05 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23944 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,33 +1,30 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(bf.readLine());
-        
-        Stack<Integer> number = new Stack<>();
-        
-        for (int i = 0; i < N; i++) {
-        	String input = bf.readLine();
-        	
-        	if (input.equals("0"))
-        		number.pop();
-        	else 
-        		number.add(Integer.parseInt(input));
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    public static void main(String[] args) throws IOException {
+        int K = Integer.parseInt(bufferedReader.readLine());
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int num = Integer.parseInt(bufferedReader.readLine());
+
+            if (num == 0) {
+                st.pop();
+                continue;
+            }
+            st.add(num);
         }
-        
-        int total = 0;
-        
-        for (int x: number)
-        	total += x;
-        
-        sb.append(total);
-        System.out.print(sb);
-	}
+
+        long total = 0;
+        while (!st.isEmpty()) {
+            total += st.pop();
+        }
+
+        System.out.println(total);
+    }
 }
