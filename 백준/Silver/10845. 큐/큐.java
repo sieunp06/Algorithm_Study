ee516e39246880ae6e480cf13b3940607a2fb49c
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-05 00:11:35 +0900

    [Silver IV] Title: 큐, Time: 168 ms, Memory: 19656 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        Deque<Integer> st = new ArrayDeque<>();
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            String command = stringTokenizer.nextToken();
+
+            if (command.equals("push")) {
+                st.add(Integer.parseInt(stringTokenizer.nextToken()));
+            }
+            if (command.equals("pop")) {
+                if (st.isEmpty()) {
+                    stringBuilder.append("-1").append("\n");
+                    continue;
+                }
+                stringBuilder.append(st.poll()).append("\n");
+            }
+            if (command.equals("size")) {
+                stringBuilder.append(st.size()).append("\n");
+            }
+            if (command.equals("empty")) {
+                if (st.isEmpty()) {
+                    stringBuilder.append(1).append("\n");
+                } else {
+                    stringBuilder.append(0).append("\n");
+                }
+            }
+            if (command.equals("front")) {
+                if (st.isEmpty()) {
+                    stringBuilder.append(-1).append("\n");
+                    continue;
+                }
+                stringBuilder.append(st.peek()).append("\n");
+            }
+            if (command.equals("back")) {
+                if (st.isEmpty()) {
+                    stringBuilder.append(-1).append("\n");
+                    continue;
+                }
+                stringBuilder.append(st.peekLast()).append("\n");
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}
