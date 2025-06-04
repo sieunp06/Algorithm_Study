785d3bab9e432b77108f07f2c1fcc78cf3282746
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-04 22:13:53 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 28692 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,23 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        Queue<Integer> q = new LinkedList<>();
-        
-        for (int i = 1; i <= N; i++) 
-            q.offer(i);
-        
-        while (q.size() > 1) {
-            q.poll();
-            q.offer(q.poll());
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            dq.add(i);
         }
-        System.out.println(q.peek());
+
+        while (dq.size() >= 2) {
+            dq.pollFirst();
+            dq.addLast(dq.pollFirst());
+
+            if (dq.size() == 1) {
+                break;
+            }
+        }
+        System.out.println(dq.poll());
     }
 }
