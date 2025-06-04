e5472659a0c763ee1a98740141b83daba247e59a
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-04 16:45:09 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 624 ms, Memory: 49788 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,48 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
-class Point implements Comparable<Point>{
-	public int x;
-	public int y;
-	
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	@Override
-	public int compareTo(Point o) {
-		if (this.y == o.y)
-			return this.x - o.x;
-		else 
-			return this.y - o.y;
-	}
-	
-}
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collector;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        
-        ArrayList<Point> answer = new ArrayList<>();
-        
-        for (int i = 0; i < N; i++) {
-        	StringTokenizer st = new StringTokenizer(bf.readLine());
-        	
-        	int x = Integer.parseInt(st.nextToken());
-        	int y = Integer.parseInt(st.nextToken());
-        	answer.add(new Point(x, y));
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    private static class Node {
+        int x, y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
-        
-        Collections.sort(answer);
-        
-        for (Point o: answer) 
-        	System.out.println(o.x + " " + o.y);
-	}
+    }
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(bufferedReader.readLine());
+
+        List<Node> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+            int x = Integer.parseInt(stringTokenizer.nextToken());
+            int y = Integer.parseInt(stringTokenizer.nextToken());
+
+            list.add(new Node(x, y));
+        }
+
+        Collections.sort(list, (o1, o2) -> {
+            if (o1.y == o2.y) {
+                return o1.x - o2.x;
+            }
+            return o1.y - o2.y;
+        });
+
+        for (Node node : list) {
+            stringBuilder.append(node.x).append(" ").append(node.y).append("\n");
+        }
+
+        System.out.println(stringBuilder);
+    }
 }
