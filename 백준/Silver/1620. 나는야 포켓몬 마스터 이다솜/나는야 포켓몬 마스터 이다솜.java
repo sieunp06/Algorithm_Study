e6471c66a4174e7d5e5188a1e391db6b59148f8d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-09 13:45:20 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 496 ms, Memory: 60720 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,36 +1,32 @@
-import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.HashMap;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        ArrayList<String> arr = new ArrayList<>();
-        
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int N = Integer.parseInt(stringTokenizer.nextToken());
+        int M = Integer.parseInt(stringTokenizer.nextToken());
+
+        Map<String, String> map1 = new HashMap<>();
+        Map<String, String> map2 = new HashMap<>();
         for (int i = 1; i <= N; i++) {
-            String input = bf.readLine();
-            map.put(input, i);
-            arr.add(input);
+            String name = bufferedReader.readLine();
+            map1.put(String.valueOf(i), name);
+            map2.put(name, String.valueOf(i));
         }
-        
-        String input;
-        
-        for (int i = 1; i <= M; i++) {
-            input = bf.readLine();
-            if (map.containsKey(input)) {	// 문자
-                System.out.println(map.get(input));
-            } else {	
-            	System.out.println(arr.get(Integer.parseInt(input) - 1));
+
+        for (int i = 0; i < M; i++) {
+            String input = bufferedReader.readLine();
+            if (map1.containsKey(input)) {
+                stringBuilder.append(map1.get(input)).append("\n");
+            } else {
+                stringBuilder.append(map2.get(input)).append("\n");
             }
         }
+        System.out.println(stringBuilder);
     }
 }
