187a07026b9bfff476f3e19abf000c7a84d70822
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-11 13:16:28 +0900

    [Silver I] Title: 절댓값 힙, Time: 272 ms, Memory: 24608 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,38 +1,32 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bf.readLine());
-        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> {
-            int first_abs = Math.abs(o1);
-            int second_abs = Math.abs(o2);
-
-            if (first_abs == second_abs) {
-                return o1 > o2 ? 1 : -1;
-            } else {
-                return first_abs - second_abs;
+        int N = Integer.parseInt(bufferedReader.readLine());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                }
+                return Integer.compare(Math.abs(o1), Math.abs(o2));
             }
         });
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(bf.readLine());
-            // x가 0이 아니라면 배열에 x를 추가함.
-            // x가 0이라면 배열세어 절댓값이 가장 작은 값을 출력하고 배열에서 그 값을 제거함.
-            if (num == 0) {
-                if (q.isEmpty()) {
-                    System.out.println("0");
-                } else {
-                    System.out.println(q.poll());
-                }
-            } else {
-                q.add(num);
+            int target = Integer.parseInt(bufferedReader.readLine());
+
+            if (target == 0) {
+                stringBuilder.append(priorityQueue.isEmpty() ? 0 : priorityQueue.poll()).append("\n");
+                continue;
             }
+            priorityQueue.add(target);
         }
+        System.out.println(stringBuilder);
     }
 }
