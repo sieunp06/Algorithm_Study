8817e3176a51dc7c18ba94e92bfd7c1007c420d0
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-11 13:05:58 +0900

    [Silver II] Title: 최소 힙, Time: 276 ms, Memory: 26976 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,28 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bf.readLine());
-
-        PriorityQueue<Integer> q = new PriorityQueue<>((o1, o2) -> o1 - o2);
+        int N = Integer.parseInt(bufferedReader.readLine());
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
 
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(bf.readLine());
+            int target = Integer.parseInt(bufferedReader.readLine());
 
-            if (num > 0) {  // num이 자연수라면
-                q.add(num);
-            } else if (num == 0) {  // num이 0이라면
-                if (q.isEmpty())
-                    System.out.println("0");
-                else
-                    System.out.println(q.poll());
+            if (target == 0) {
+                stringBuilder.append(priorityQueue.isEmpty() ? 0 : priorityQueue.poll()).append("\n");
+                continue;
             }
+            priorityQueue.add(target);
         }
+        System.out.println(stringBuilder);
     }
 }
