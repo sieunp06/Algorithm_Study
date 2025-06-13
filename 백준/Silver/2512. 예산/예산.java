a862c896a831332718a4612cc4b7b9f75002e094
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-14 00:02:44 +0900

    [Silver II] Title: 예산, Time: 160 ms, Memory: 17044 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,54 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N, M;
-    private static int[] requests;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-    private static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    private static StringTokenizer stringTokenizer;
+    static int N, M, max;
+    static int[] regions;
 
     public static void main(String[] args) throws IOException {
         init();
-        System.out.println(search());
+        System.out.println(find());
     }
 
     private static void init() throws IOException {
         N = Integer.parseInt(bufferedReader.readLine());
-        requests = new int[N];
+        regions = new int[N];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        for (int i = 0; i < N ;i++) {
-            requests[i] = Integer.parseInt(stringTokenizer.nextToken());
-        }
-        M = Integer.parseInt(bufferedReader.readLine());
 
-        Arrays.sort(requests);
+        max = Integer.MIN_VALUE;
+        for (int i = 0 ; i < N; i++) {
+            regions[i] = Integer.parseInt(stringTokenizer.nextToken());
+            max = Math.max(max, regions[i]);
+        }
+        Arrays.sort(regions);
+
+        M = Integer.parseInt(bufferedReader.readLine());
     }
 
-    private static int search() {
-        int left = 0; int right = requests[N - 1];
-        int result = 0;
-
+    private static int find() {
+        int left = 0; int right = max;
+        int result = -1;
         while (left <= right) {
             int mid = (left + right) / 2;
-            if (totalBudget(mid) > M) {
-                right = mid - 1;
-            } else {
+            if (sumTotal(mid) <= M) {
                 result = mid;
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
         return result;
     }
 
-    private static int totalBudget(int standard) {
+    private static int sumTotal(int mid) {
         int total = 0;
-        for (int num : requests) {
-            total += Math.min(num, standard);
+        for (int region : regions) {
+            total += (Math.min(region, mid));
         }
         return total;
     }
