3b6d64cd92404348e53af5f3c428502f047717eb
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-15 18:04:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,26 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] numbers;
-
     static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static final StringBuilder stringBuilder = new StringBuilder();
     static StringTokenizer stringTokenizer;
 
-    public static void main(String[] args) throws IOException{
+    private static int N;
+    private static int[] numbers, dp;
+
+    public static void main(String[] args) throws IOException {
         init();
-        System.out.println(solve());
+        findByDp();
+        System.out.println(findMax());
     }
 
-    static void init() throws IOException {
+    private static void init() throws IOException {
         N = Integer.parseInt(bufferedReader.readLine());
-
         numbers = new int[N];
+        dp = new int[N];
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         for (int i = 0; i < N; i++) {
@@ -28,18 +26,22 @@ public class Main {
         }
     }
 
-    static int solve() {
-        int[] dp = new int[N];
-
-        for (int i = 0; i < N; i++) {
-            dp[i] = 1;
+    private static void findByDp() {
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < N; i++) {
             for (int j = 0; j < i; j++) {
                 if (numbers[j] < numbers[i]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
         }
+    }
 
-        return Arrays.stream(dp).max().getAsInt();
+    private static int findMax() {
+        int max = dp[0];
+        for (int i = 1; i < N; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        return max;
     }
 }
