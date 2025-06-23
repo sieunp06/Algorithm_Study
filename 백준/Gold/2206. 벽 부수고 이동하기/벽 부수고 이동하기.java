48f347b76c28451cf6102b9cc1f8ad1b028f4392
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-24 01:32:34 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 636 ms, Memory: 107088 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,86 +2,86 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
-    static boolean[][][] visited;
-
-    static class Node {
-        int r, c, distance;
-        boolean broken;
-
-        Node(int r, int c, int distance, boolean broken) {
-            this.r = r;
-            this.c = c;
-            this.distance = distance;
-            this.broken = broken;
-        }
-    }
-
-    // 상하좌우
-    static int[] dr = {-1, 1, 0, 0};
-    static int[] dc = {0, 0, -1, 1};
-
     static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static final StringBuilder stringBuilder = new StringBuilder();
     static StringTokenizer stringTokenizer;
+
+    private static int N, M;
+    private static int[][] board;
+    private static boolean[][][] visited;
+
+    private static int[] dr = {-1, 1, 0, 0};
+    private static int[] dc = {0, 0, -1, 1};
+
+    private static class Node {
+        int r, c, dist, broken;
+
+        Node(int r, int c, int dist, int broken) {
+            this.r = r;
+            this.c = c;
+            this.dist = dist;
+            this.broken = broken;
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         init();
         System.out.println(bfs());
     }
 
-    static void init() throws IOException {
+    private static void init() throws IOException {
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
         N = Integer.parseInt(stringTokenizer.nextToken());
         M = Integer.parseInt(stringTokenizer.nextToken());
 
-        map = new int[N][M];
+        board = new int[N][M];
         visited = new boolean[N][M][2];
 
         for (int i = 0; i < N; i++) {
             String input = bufferedReader.readLine();
             for (int j = 0; j < M; j++) {
-                map[i][j] = input.charAt(j) - '0';
+                board[i][j] = input.charAt(j) - '0';
             }
         }
     }
 
-    static int bfs() {
+    private static int bfs() {
         Queue<Node> queue = new ArrayDeque<>();
-        queue.add(new Node(0, 0, 1, false));
+        queue.add(new Node(0, 0, 1, 0));
         visited[0][0][0] = true;
 
         while (!queue.isEmpty()) {
             Node now = queue.poll();
+            int r = now.r;
+            int c = now.c;
 
-            if (now.r == N - 1 && now.c == M - 1) return now.distance;
+            if (r == N - 1 && c == M - 1) {
+                return now.dist;
+            }
 
             for (int i = 0; i < 4; i++) {
-                int nr = now.r + dr[i];
-                int nc = now.c + dc[i];
+                int nr = r + dr[i];
+                int nc = c + dc[i];
 
                 if (!isIn(nr, nc)) continue;
 
-                if (map[nr][nc] == 1) {
-                    if (!now.broken && !visited[nr][nc][1]) {
-                        visited[nr][nc][1] = true;
-                        queue.add(new Node(nr, nc, now.distance + 1, true));
-                    }
-                } else {
-                    int brokenIdx = now.broken ? 1 : 0;
-                    if (!visited[nr][nc][brokenIdx]) {
-                        visited[nr][nc][brokenIdx] = true;
-                        queue.add(new Node(nr, nc, now.distance + 1, now.broken));
-                    }
+                // 벽이 아닐 때
+                if (board[nr][nc] == 0 && !visited[nr][nc][now.broken]) {
+                    visited[nr][nc][now.broken] = true;
+                    queue.add(new Node(nr, nc, now.dist + 1, now.broken));
+                }
+
+                // 벽인데 아직 부순 적 없을 때
+                if (board[nr][nc] == 1 && now.broken == 0 && !visited[nr][nc][1]) {
+                    visited[nr][nc][1] = true;
+                    queue.add(new Node(nr, nc, now.dist + 1, 1));
                 }
             }
         }
         return -1;
     }
 
-    static boolean isIn(int r, int c) {
-        return r >= 0 && c >= 0 && r < N && c < M;
+    private static boolean isIn(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < M;
     }
 }
