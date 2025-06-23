ee284cfb7874acc7ba6cec94af17b425dc2056a0
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-23 22:50:58 +0900

    [Gold V] Title: 적록색약, Time: 140 ms, Memory: 17260 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -9,7 +9,6 @@ public class Main {
     private static int answerArea = 0;
     private static int answerColorWeakness = 0;
 
-    // 상하좌우
     private static int[] dr = {-1, 1, 0, 0};
     private static int[] dc = {0, 0, -1, 1};
 
