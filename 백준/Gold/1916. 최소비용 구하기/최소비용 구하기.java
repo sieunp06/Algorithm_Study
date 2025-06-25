41c2fc28df5b7e91af29c141ea5bf7caccb37524
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-25 16:23:03 +0900

    [Gold V] Title: 최소비용 구하기, Time: 380 ms, Memory: 50428 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,16 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
-    static int start, end;
-    static List<Node>[] nodes;
-    static int[] dist;
-    static final int INF = Integer.MAX_VALUE;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-    static class Node implements Comparable<Node> {
+    private static int N, M, from, to;
+    private static List<Node>[] nodes;
+    private static int[] dist;
+    private static final int INF = Integer.MAX_VALUE;
+
+    private static class Node implements Comparable<Node> {
         int to, cost;
 
         Node(int to, int cost) {
@@ -20,35 +21,29 @@ public class Main {
 
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            return Integer.compare(this.cost, o.cost);
         }
     }
 
-    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static final StringBuilder stringBuilder = new StringBuilder();
-    static StringTokenizer stringTokenizer;
-
     public static void main(String[] args) throws IOException {
         init();
-        solve();
-        System.out.println(dist[end]);
+        System.out.println(dijkstra(from));
     }
 
-    static void init() throws IOException {
+    private static void init() throws IOException {
         N = Integer.parseInt(bufferedReader.readLine());
         M = Integer.parseInt(bufferedReader.readLine());
 
         nodes = new List[N + 1];
+        dist = new int[N + 1];
+        Arrays.fill(dist, INF);
+
         for (int i = 1; i <= N; i++) {
             nodes[i] = new ArrayList<>();
         }
 
-        dist = new int[N + 1];
-        Arrays.fill(dist, INF);
-
         for (int i = 0; i < M; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
             int from = Integer.parseInt(stringTokenizer.nextToken());
             int to = Integer.parseInt(stringTokenizer.nextToken());
             int cost = Integer.parseInt(stringTokenizer.nextToken());
@@ -57,29 +52,29 @@ public class Main {
         }
 
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        start = Integer.parseInt(stringTokenizer.nextToken());
-        end = Integer.parseInt(stringTokenizer.nextToken());
+        from = Integer.parseInt(stringTokenizer.nextToken());
+        to = Integer.parseInt(stringTokenizer.nextToken());
     }
 
-    static void solve() {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
+    private static int dijkstra(int start) {
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Node(start, 0));
         dist[start] = 0;
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
+        while (!priorityQueue.isEmpty()) {
+            Node node = priorityQueue.poll();
 
-            int to = now.to;
-            int cost = now.cost;
+            if (node.to == to) return dist[to];
+            if (node.cost > dist[node.to]) continue;
 
-            if (cost > dist[now.to]) continue;
-
-            for (Node node : nodes[to]) {
-                if (dist[node.to] > dist[to] + node.cost) {
-                    dist[node.to] = dist[to] + node.cost;
-                    pq.offer(new Node(node.to, dist[node.to]));
+            for (Node next : nodes[node.to]) {
+                if (dist[next.to] > dist[node.to] + next.cost) {
+                    dist[next.to] = dist[node.to] + next.cost;
+                    priorityQueue.add(new Node(next.to, dist[next.to]));
                 }
             }
         }
+
+        return dist[to];
     }
 }
