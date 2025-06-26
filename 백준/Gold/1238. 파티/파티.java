7ac306708392167fb504b6d1365e7ba080ea5875
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-06-26 23:35:47 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 18644 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,17 +1,20 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, X;
-    static List<Node>[] nodes, nodesReverse;
-    static int[] answer;
-    static int[] dist;
-    static final int INF = Integer.MAX_VALUE;
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-    static class Node implements Comparable<Node> {
+    private static int N, M, X;
+    private static List<Node>[] graph;
+    private static List<Node>[] reverseGraph;
+    private static int[] distance;
+    private static final int INF = Integer.MAX_VALUE;
+
+    private static class Node implements Comparable<Node> {
         int to, cost;
+
         Node(int to, int cost) {
             this.to = to;
             this.cost = cost;
@@ -19,19 +22,14 @@ public class Main {
 
         @Override
         public int compareTo(Node o) {
-            return this.cost - o.cost;
+            return Integer.compare(cost, o.cost);
         }
     }
 
-    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    static final StringBuilder stringBuilder = new StringBuilder();
-    static StringTokenizer stringTokenizer;
-
     public static void main(String[] args) throws IOException {
         init();
-
-        int[] distTo = solve(X, nodes);
-        int[] distFrom = solve(X, nodesReverse);
+        int[] distTo = dijkstra(X, graph);
+        int[] distFrom = dijkstra(X, reverseGraph);
 
         int answer = Integer.MIN_VALUE;
         for (int i = 1; i <= N; i++) {
@@ -41,54 +39,51 @@ public class Main {
         System.out.println(answer);
     }
 
-    static void init() throws IOException {
+    private static void init() throws IOException {
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         N = Integer.parseInt(stringTokenizer.nextToken());
         M = Integer.parseInt(stringTokenizer.nextToken());
         X = Integer.parseInt(stringTokenizer.nextToken());
 
-        nodes = new List[N + 1];
-        nodesReverse = new List[N + 1];
+        graph = new List[N + 1];
+        reverseGraph = new List[N + 1];
+
         for (int i = 1; i <= N; i++) {
-            nodes[i] = new ArrayList<>();
-            nodesReverse[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
+            reverseGraph[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < M; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-
             int from = Integer.parseInt(stringTokenizer.nextToken());
             int to = Integer.parseInt(stringTokenizer.nextToken());
             int cost = Integer.parseInt(stringTokenizer.nextToken());
 
-            nodes[from].add(new Node(to, cost));
-            nodesReverse[to].add(new Node(from, cost));
+            graph[from].add(new Node(to, cost));
+            reverseGraph[to].add(new Node(from, cost));
         }
     }
 
-    static int[] solve(int start, List<Node>[] nodes) {
-        dist = new int[N + 1];
-        Arrays.fill(dist, INF);
+    private static int[] dijkstra(int start, List<Node>[] nodes) {
+        distance = new int[N + 1];
+        Arrays.fill(distance, INF);
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.add(new Node(start, 0));
-        dist[start] = 0;
+        PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(new Node(start, 0));
+        distance[start] = 0;
 
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
+        while (!priorityQueue.isEmpty()) {
+            Node now = priorityQueue.poll();
 
-            int to = now.to;
-            int cost = now.cost;
+            if (now.cost > distance[now.to]) continue;
 
-            if (cost > dist[to]) continue;
-
-            for (Node node : nodes[to]) {
-                if (dist[node.to] > dist[to] + node.cost) {
-                    dist[node.to] = dist[to] + node.cost;
-                    pq.offer(new Node(node.to, dist[node.to]));
+            for (Node node : nodes[now.to]) {
+                if (distance[node.to] > distance[now.to] + node.cost) {
+                    distance[node.to] = distance[now.to] + node.cost;
+                    priorityQueue.add(new Node(node.to, distance[node.to]));
                 }
             }
         }
-        return dist;
+        return distance;
     }
 }
