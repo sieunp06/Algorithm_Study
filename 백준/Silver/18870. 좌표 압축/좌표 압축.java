550a9916fe9c86dd38a8fe4dc2b7dc5fb175217a
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-07-03 22:54:46 +0900

    [Silver II] Title: 좌표 압축, Time: 1696 ms, Memory: 264544 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,44 +1,50 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        int[] arr = new int[N];
-        int[] sorted = new int[N];
-        
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        for (int i = 0; i < N; i++) {
-        	String input = st.nextToken();
-        	sorted[i] = arr[i] = Integer.parseInt(input);
-        }
-        
-        Arrays.sort(sorted);
-        
-        int rank = 0;
-        
-        for (int x: sorted) {
-        	if (!map.containsKey(x)) {
-        		map.put(x, rank);
-        		rank++;
-        	}
-        }
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
 
-        StringBuilder sb = new StringBuilder();
-        
-        for (int x: arr) {
-        	sb.append(map.get(x)).append(' ');
+    static int N;
+    static int[] numbers;
+    static int[] sortedNumbers;
+    static Map<Integer, Integer> compressedMap;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        compressCoordinates();
+        printCompressedCoordinates();
+        System.out.println(stringBuilder);
+    }
+
+    static void init() throws IOException {
+        N = Integer.parseInt(bufferedReader.readLine());
+        numbers = new int[N];
+        sortedNumbers = new int[N];
+
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(stringTokenizer.nextToken());
+            sortedNumbers[i] = numbers[i];
         }
-        
-        System.out.println(sb);
-	}
+    }
+
+    static void compressCoordinates() {
+        Arrays.sort(sortedNumbers);
+        compressedMap = new HashMap<>();
+
+        int rank = 0;
+        for (int num : sortedNumbers) {
+            if (!compressedMap.containsKey(num)) {
+                compressedMap.put(num, rank++);
+            }
+        }
+    }
+
+    static void printCompressedCoordinates() {
+        for (int num : numbers) {
+            stringBuilder.append(compressedMap.get(num)).append(" ");
+        }
+    }
 }
