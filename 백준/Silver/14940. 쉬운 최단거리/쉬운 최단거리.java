16d96387d7b838f1b1037c77295770838282c6f0
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-07-03 23:24:21 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 536 ms, Memory: 45276 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,83 +1,83 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    private static int n, m;
-    private static int[][] boards;
-    private static boolean[][] visited;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, 1, -1};
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    static int n, m;
+    static int[][] map;
+    static int[][] distance;
+    static boolean[][] visited;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
+
+    static Queue<int[]> queue = new ArrayDeque<>();
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer;
+        init();
+        bfs();
+        printResult();
+    }
 
+    static void init() throws IOException {
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
         n = Integer.parseInt(stringTokenizer.nextToken());
         m = Integer.parseInt(stringTokenizer.nextToken());
 
-        boards = new int[n][m];
+        map = new int[n][m];
+        distance = new int[n][m];
         visited = new boolean[n][m];
 
-        int x = 0; int y = 0;
         for (int i = 0; i < n; i++) {
             stringTokenizer = new StringTokenizer(bufferedReader.readLine());
             for (int j = 0; j < m; j++) {
-                boards[i][j] = Integer.parseInt(stringTokenizer.nextToken());
-                if (boards[i][j] == 2) {
-                    boards[i][j] = 0;
-                    x = i; y = j;
+                map[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                if (map[i][j] == 2) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
                 }
             }
         }
+    }
 
-        bfs(x, y);
+    static void bfs() {
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int r = cur[0];
+            int c = cur[1];
 
-        StringBuilder stringBuilder = new StringBuilder();
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d];
+                int nc = c + dc[d];
+
+                if (isIn(nr, nc) && !visited[nr][nc] && map[nr][nc] == 1) {
+                    queue.add(new int[]{nr, nc});
+                    visited[nr][nc] = true;
+                    distance[nr][nc] = distance[r][c] + 1;
+                }
+            }
+        }
+    }
+
+    static boolean isIn(int r, int c) {
+        return r >= 0 && r < n && c >= 0 && c < m;
+    }
+
+    static void printResult() {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (boards[i][j] == 1 && !visited[i][j]) {
-                    boards[i][j] = -1;
+                if (map[i][j] == 0) {
+                    stringBuilder.append(0).append(" ");
+                } else if (!visited[i][j]) {
+                    stringBuilder.append(-1).append(" ");
+                } else {
+                    stringBuilder.append(distance[i][j]).append(" ");
                 }
-                stringBuilder.append(boards[i][j]).append(" ");
             }
             stringBuilder.append("\n");
         }
-
-        System.out.println(stringBuilder);
-    }
-
-    private static void bfs(int x, int y) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y));
-        visited[x][y] = true;
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-                    continue;
-                }
-                if (visited[nx][ny] || boards[nx][ny] == 0) {
-                    continue;
-                }
-                q.add(new Node(nx, ny));
-                visited[nx][ny] = true;
-                boards[nx][ny] = boards[node.x][node.y] + 1;
-            }
-        }
-    }
-}
-
-class Node {
-    int x;
-    int y;
-    Node(int x, int y) {
-        this.x = x;
-        this.y = y;
+        System.out.print(stringBuilder);
     }
 }
