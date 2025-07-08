0a55d4b7e42eb73a867326fc752f40a08d7ce38d
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-07-09 04:41:00 +0900

    [Gold V] Title: AC, Time: 672 ms, Memory: 90500 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,87 +1,78 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int T, n;
-    static String p, input;
-    static Deque<Integer> numbers;
-
-    static boolean order;
-    static boolean isError;
-
     static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     static final StringBuilder stringBuilder = new StringBuilder();
     static StringTokenizer stringTokenizer;
 
+    static int n;
+    static String p;
+    static Deque<Integer> deque;
+    static boolean reverse;
+
     public static void main(String[] args) throws IOException {
-        T = Integer.parseInt(bufferedReader.readLine());
-        while (T-- > 0) {
+        int T = Integer.parseInt(bufferedReader.readLine());
+        for (int tc = 1; tc <= T; tc++) {
             init();
-            precess();
-            printResult();
+            solve();
         }
-        System.out.println(stringBuilder);
     }
 
     static void init() throws IOException {
         p = bufferedReader.readLine();
         n = Integer.parseInt(bufferedReader.readLine());
 
-        numbers = new ArrayDeque<>();
-        order = true;
-        isError = false;
+        deque = new ArrayDeque<>();
+        String input = bufferedReader.readLine();
+        String[] num = input.substring(1, input.length() - 1).split(",");
 
-        input = bufferedReader.readLine();
-        input = input.substring(1, input.length() - 1);
-        String[] inputs = input.split(",");
-
-        for (int i = 0; i < n; i++) {
-            numbers.add(Integer.parseInt(inputs[i]));
+        if (n > 0) {
+            for (int i = 0; i < num.length; i++) {
+                deque.add(Integer.parseInt(num[i]));
+            }
         }
     }
 
-    static void precess() {
-        for (char alpha : p.toCharArray()) {
-            if (alpha == 'R') { // 배열의 순서 뒤집기
-                order = !order;
-            }
-            if (alpha == 'D') { // 첫 번째 수 버리기
-                if (numbers.isEmpty()) {
-                    isError = true;
-                    break;
+    static void solve() {
+        reverse = false;
+        for (char command : p.toCharArray()) {
+            if (command == 'R') {
+                reverse = !reverse;
+            } else if (command == 'D') {
+                if (deque.isEmpty()) {
+                    System.out.println("error");
+                    return;
                 }
-                if (order) {    // 정방향
-                    numbers.pollFirst();
+                if (reverse) {
+                    deque.pollLast();
                 } else {
-                    numbers.pollLast();
+                    deque.pollFirst();
                 }
             }
         }
+        printResult();
     }
 
     static void printResult() {
-        if (isError) {
-            stringBuilder.append("error").append("\n");
-        } else {
-            stringBuilder.append("[");
-            while (!numbers.isEmpty()) {
-                if (order) {
-                    if (numbers.size() == 1) {
-                        stringBuilder.append(numbers.pollFirst());
-                    } else {
-                        stringBuilder.append(numbers.pollFirst()).append(",");
-                    }
-                } else {
-                    if (numbers.size() == 1) {
-                        stringBuilder.append(numbers.pollLast());
-                    } else {
-                        stringBuilder.append(numbers.pollLast()).append(",");
-                    }
+        stringBuilder.setLength(0);
+        stringBuilder.append("[");
+        if (!deque.isEmpty()) {
+            if (reverse) {
+                Iterator<Integer> iter = deque.descendingIterator();
+                stringBuilder.append(iter.next());
+                while (iter.hasNext()) {
+                    stringBuilder.append(",").append(iter.next());
+                }
+            } else {
+                Iterator<Integer> iter = deque.iterator();
+                stringBuilder.append(iter.next());
+                while (iter.hasNext()) {
+                    stringBuilder.append(",").append(iter.next());
                 }
             }
-            stringBuilder.append("]").append("\n");
         }
+        stringBuilder.append("]");
+        System.out.println(stringBuilder);
     }
 }
