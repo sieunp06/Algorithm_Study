79bb1c842882fac809335382f54c58afecb26889
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-08-09 10:04:38 +0900

    [level 2] Title: 타겟 넘버, Time: 4.35 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,23 @@
 class Solution {
-    int answer = 0;
+    private int answer = 0;
+    
     public int solution(int[] numbers, int target) {
-        DFS(0, 0, numbers, target);
+        
+        dfs(numbers, 1, numbers[0], target);
+        dfs(numbers, 1, -numbers[0], target);
+        
         return answer;
     }
     
-    private void DFS(int depth, int total, int[] numbers, int target) {
-        if (depth == numbers.length) {
+    private void dfs(int[] numbers, int index, int total, int target) {
+        if (index == numbers.length) {
             if (total == target) {
                 answer++;
             }
             return;
         }
-        DFS(depth + 1, total + numbers[depth], numbers, target);
-        DFS(depth + 1, total - numbers[depth], numbers, target);
+        
+        dfs(numbers, index + 1, total + numbers[index], target);
+        dfs(numbers, index + 1, total - numbers[index], target);
     }
 }
