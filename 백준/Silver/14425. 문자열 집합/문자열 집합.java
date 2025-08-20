58fec2a93a0365c2e6729a3940cf072932aae208
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-08-20 14:19:08 +0900

    [Silver IV] Title: 문자열 집합, Time: 2392 ms, Memory: 872964 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,28 +1,69 @@
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.util.HashMap;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < N; i++) 
-            map.put(bf.readLine(), 0);
-        
-        int answer = 0;
-        
-        for (int i = 0; i < M; i++) {
-            if (map.containsKey(bf.readLine())) answer++;
-        }
-        
-        System.out.println(answer);
-    }
+	
+	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+	static StringBuilder stringBuilder = new StringBuilder();
+	static StringTokenizer stringTokenizer;
+	
+	private static int N, M;
+	
+	private static class TrieNode {
+		TrieNode[] child = new TrieNode['z' - 'a' + 1];
+		boolean isEnd;
+	}
+	
+	private static class Trie {
+		TrieNode root = new TrieNode();
+		
+		void insert(String word) {
+			TrieNode cur = root;
+			for (int i = 0; i < word.length(); i++) {
+				int idx = word.charAt(i) - 'a';
+				if (cur.child[idx] == null) {
+					cur.child[idx] = new TrieNode();
+				}
+				cur = cur.child[idx];
+			}
+			cur.isEnd = true;
+			
+		}
+		
+		boolean search(String word) {
+			TrieNode cur = root;
+			for (int i = 0; i < word.length(); i++) {
+				int idx = word.charAt(i) - 'a';
+				if (cur.child[idx] == null) {
+					return false;
+				}
+				cur = cur.child[idx];
+			}
+			return cur.isEnd;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+		
+		N = Integer.parseInt(stringTokenizer.nextToken());
+		M = Integer.parseInt(stringTokenizer.nextToken());
+		
+		Trie trie = new Trie();
+		
+		for (int i = 0; i < N; i++) {
+			trie.insert(bufferedReader.readLine());
+		}
+		
+		int count = 0;
+		for (int i = 0; i < M; i++) {
+			if (trie.search(bufferedReader.readLine())) {
+				count++;
+			}
+		}	
+		
+		System.out.println(count);
+	}
 }
