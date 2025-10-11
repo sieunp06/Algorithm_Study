55f5719b8db5b4cd9a92521857695e9664587eed
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-10-11 13:54:03 +0900

    [Gold III] Title: 소수의 연속합, Time: 172 ms, Memory: 27036 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -2,44 +2,55 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
+    static final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder stringBuilder = new StringBuilder();
+    static StringTokenizer stringTokenizer;
+
+    static int N;
+    static List<Integer> primes = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        
-        int answer = 0;
-        
-        ArrayList<Integer> number = new ArrayList<>();
-        
-        int[] isPrime = new int[N + 1];
-        
+        init();
+        System.out.println(process());
+    }
+
+    private static void init() throws IOException {
+        N = Integer.parseInt(bufferedReader.readLine());
+        generatePrimeNumbers();
+    }
+
+    private static void generatePrimeNumbers() {
+        boolean[] isPrime = new boolean[N + 1];
+        for (int i = 2; i * i <= N; i++) {
+            if (!isPrime[i]) {
+                for (int j = i * i; j <= N; j += i) {
+                    isPrime[j] = true;
+                }
+            }
+        }
         for (int i = 2; i <= N; i++) {
-        	if (isPrime[i] == 0) {
-        		number.add(i);
-        		for (int j = i; j <= N; j = j + i) {
-        			isPrime[j] = 1;
-        		}
-        	}
+            if (!isPrime[i]) primes.add(i);
         }
-        
-        int sum = 0;
-        int lt = 0;
-        
-        for (int rt = 0; rt < N; rt++) {
-        	if (rt == number.size()) break;
-        	sum += number.get(rt);
-        	if (sum == N) answer++;
-        	while (sum >= N) {
-        		sum -= number.get(lt++);
-        		if (sum == N) {
-        			answer++;
-        		}
-        	}
+    }
+
+    private static int process() {
+        int count = 0;
+        int start = 0; int end = 0;
+        int total = 0;
+
+        while (true) {
+            if (total >= N) {
+                if (total == N) count++;
+                total -= primes.get(start++);
+            } else {
+                if (end == primes.size()) break;
+                total += primes.get(end++);
+            }
         }
-        
-        System.out.println(answer);
-        bf.close();
+        return count;
     }
 }
