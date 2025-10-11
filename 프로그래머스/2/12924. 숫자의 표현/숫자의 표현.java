acfcefd93ae5a6080c6fd29044f0d7d334170b77
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-10-11 09:32:31 +0900

    [level 2] Title: 숫자의 표현, Time: 0.58 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,25 +1,21 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        int[] numbers = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            numbers[i] = i + numbers[i - 1];
-        }
-        
         int start = 1; int end = 1;
+        int total = 1;
         
-        while (end < n + 1) {
-            int target = numbers[end] - numbers[start - 1];
-            if (target == n) {
+        while (start <= n) {
+            if (total == n) {
                 answer++;
-                start++; end++;
-            } else if (target > n) {
+                total -= start;
                 start++;
-            } else {
+            } else if (total < n) {
                 end++;
+                total += end;
+            } else {
+                total -= start;
+                start++;
             }
         }
         
