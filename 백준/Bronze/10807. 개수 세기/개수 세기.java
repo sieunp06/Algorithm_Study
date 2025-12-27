dcf8c0c2378f505b8cc850f21cda350147d85548
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-12-28 01:30:04 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,29 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        
-        int[] numbers = new int[N];
-        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-        
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int v = Integer.parseInt(bufferedReader.readLine());
+
+        int answer = 0;
         for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            if (v == Integer.parseInt(stringTokenizer.nextToken())) {
+                answer++;
+            }
         }
-        
-        int num = Integer.parseInt(bf.readLine());
-        int count = 0;
-        
-        for (int i = 0; i < numbers.length; i++) {
-            if (num == numbers[i])
-                count ++;
-        }
-        
-        System.out.println(count);
+        System.out.println(answer);
     }
 }
