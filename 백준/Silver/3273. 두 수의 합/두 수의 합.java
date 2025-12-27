e8252ae1b504acf879757f7b885b4d9665ece550
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-12-28 01:26:04 +0900

    [Silver III] Title: 두 수의 합, Time: 304 ms, Memory: 27316 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,39 +1,36 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(bf.readLine());
-        int[] numbers = new int[N];
-        
-        StringTokenizer st = new StringTokenizer(bf.readLine());
-        
-        for (int i = 0; i < N; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine());
+        StringTokenizer input = new StringTokenizer(bufferedReader.readLine());
+        int[] inputs = new int[n];
+        for (int i = 0; i < n; i++) {
+            inputs[i] = Integer.parseInt(input.nextToken());
         }
-        Arrays.sort(numbers);
-        
-        int x = Integer.parseInt(bf.readLine());
-        
+        int x = Integer.parseInt(bufferedReader.readLine());
+        Arrays.sort(inputs);
+
+        int start = 0; int end = n - 1;
         int answer = 0;
-        int sum;
-        int lt = 0;
-        int rt = N - 1;
-        
-        while (lt < rt) {
-            sum = numbers[lt] + numbers[rt];
-            if (sum == x) answer++;
-            if (sum <= x) lt++;
-            else rt--;
+        while (start < end) {
+            int total = inputs[start] + inputs[end];
+            if (total == x) {
+                answer++;
+                start++; end--;
+            } else if (total < x) {
+                start++;
+            } else {
+                end--;
+            }
         }
+
         System.out.println(answer);
-        
-        bf.close();
     }
 }
