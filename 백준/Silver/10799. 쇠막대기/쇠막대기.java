0ad2656c18abc937360c17a9826c6adfbc25f3df
Sieun Park <87406514+sieunp06@users.noreply.github.com>
2025-12-30 00:06:48 +0900

    [Silver II] Title: 쇠막대기, Time: 144 ms, Memory: 15708 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,34 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String text = bufferedReader.readLine();
-        
+        String input = bufferedReader.readLine();
         int answer = 0;
-        int status = 0;
+        int count = 0;
 
-        char[] arr = text.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            char now = arr[i];
-            if (now == '(') {
-                status++;
-            }
-            if (now == ')') {
-                if (status > 0) {
-                    status--;
-                }
-                if (arr[i - 1] == '(') {
-                    answer += status;
+        for (int i = 0; i < input.length(); i++) {
+            char target = input.charAt(i);
+
+            if (target == '(') {
+                count++;
+            } else {
+                if (input.charAt(i - 1) == '(') {
+                    count--;
+                    answer += count;
                 } else {
+                    count--;
                     answer += 1;
                 }
             }
         }
-
-        System.out.println(answer);
+        System.out.print(answer);
     }
 }
